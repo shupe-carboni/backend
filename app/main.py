4192fabd2df4e777d955723be1952dc1c824f122
carboni-joseph <joe@carbonitech.com>
2023-10-23 21:46:08 -0400

    check content type in req only for reqs with body

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI, Header, HTTPException, Depends
-from typing import Annotated
+from fastapi import FastAPI, Header, HTTPException, Depends, Request
+from typing import Annotated, Optional
 from starlette.responses import RedirectResponse
 from app.quotes.quotes import quotes
 from app.jsonapi import JSONAPI_CONTENT_TYPE
 
 app = FastAPI()
 
-async def application_vnd_json(content_type: Annotated[str, Header()]) -> None:
-    if content_type != JSONAPI_CONTENT_TYPE:
+async def application_vnd_json(
+        request: Request,
+        content_type: Annotated[str, Header()]=None,
+    ) -> None:
+    if request.method in ('GET', 'DELETE'):
+        return
+    elif content_type != JSONAPI_CONTENT_TYPE:
         raise HTTPException(status_code=415)
 
 vnd_json = Depends(application_vnd_json)
