f150e4cd5607c59304ad23f9af0d21140e0c6547
carboni-joseph <joe@carbonitech.com>
2023-11-03 13:02:20 -0400

    remove header requirement

--- app/main.py
+++ app/main.py
@@ -1,23 +1,10 @@
-from fastapi import FastAPI, Header, HTTPException, Depends, Request
-from typing import Annotated, Optional
+from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from app.quotes.quotes import quotes
-from app.jsonapi import JSONAPI_CONTENT_TYPE
 
 app = FastAPI()
 
-async def application_vnd_json(
-        request: Request,
-        content_type: Annotated[str, Header()]=None,
-    ) -> None:
-    if request.method in ('GET', 'DELETE'):
-        return
-    elif content_type != JSONAPI_CONTENT_TYPE:
-        raise HTTPException(status_code=415)
-
-vnd_json = Depends(application_vnd_json)
-depends = [vnd_json]
-app.include_router(quotes, dependencies=depends)
+app.include_router(quotes)
 
 @app.get('/')
 async def home():
