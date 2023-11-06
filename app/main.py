cc300234b5d355875d4cc4cadb7889b9642a4608
carboni-joseph <joe@carbonitech.com>
2023-11-06 14:40:31 -0500

    include relationships and vendors routers

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
-from app.quotes.quotes import quotes
+## Routers ##
+from app import relationships
+from app.quotes import quotes
+from app.vendors import vendors
 
 app = FastAPI()
 
+app.include_router(relationships)
+app.include_router(vendors)
 app.include_router(quotes)
 
 @app.get('/')
