87ef70fcdd96333586ace46206a987cfd61d0817
carboni-joseph <joe@carbonitech.com>
2023-11-07 17:07:43 -0500

    formatting

--- app/quotes/quotes.py
+++ app/quotes/quotes.py
@@ -14,14 +14,14 @@ from app.quotes.models import (
 quotes = APIRouter(prefix='/quotes', tags=['quotes'])
 
 @quotes.get('', response_model=QuoteResponse)
-async def quotes_collection(
+async def quote_collection(
         query: QuoteQuery=Depends(),
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> QuoteResponse:
     raise HTTPException(status_code=501)
 
 @quotes.get('/{quote_id}')
-async def quote_detail(
+async def quote(
         quote_id: int,
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> QuoteResponse:
@@ -30,31 +30,36 @@ async def quote_detail(
 @quotes.delete('/{quote_id}')
 async def delete_quote(
         quote_id: int,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> None:
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> None:
     raise HTTPException(status_code=501)
 
 @quotes.post('')
 async def new_quote(
         body: NewQuoteResourceObject,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> QuoteResponse:
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> QuoteResponse:
     raise HTTPException(status_code=501)
 
 @quotes.post('/{quote_id}/products')
 async def add_products(
         body: NewQuoteDetailResourceObject,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> QuoteDetailResponse:
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> QuoteDetailResponse:
     raise HTTPException(status_code=501)
 
 @quotes.patch('/{quote_id}')
 async def modify_quote(
         quote_id: int,
         body: ExistingQuote,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> QuoteResponse:
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> QuoteResponse:
     raise HTTPException(status_code=501)
 
 @quotes.patch('/{quote_id}/products')
 async def modify_products(
         quote_id: int,
         body: ExistingQuoteDetail,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> QuoteDetailResponse:
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> QuoteDetailResponse:
     raise HTTPException(status_code=501)
