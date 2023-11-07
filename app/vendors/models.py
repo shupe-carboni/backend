93b2b2549e4196aa58f964383415e1644090290f
carboni-joseph <joe@carbonitech.com>
2023-11-07 17:07:58 -0500

    build out endpoints & models

--- app/vendors/models.py
+++ app/vendors/models.py
@@ -1,16 +1,57 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, create_model
+from typing import Optional
+from app.jsonapi import JSONAPIRelationships, JSONAPIResourceObject, Pagination, JSONAPIResourceIdentifier, Query
 
+## Vendor
+class VendorAttributes(BaseModel):
+    name: str
+    headquarters: str
+    description: str
+    phone: int
+
+class VendorRelationships(BaseModel):
+    info:  JSONAPIRelationships
+
+class VendorResourceObject(JSONAPIResourceIdentifier):
+    attributes: VendorAttributes
+    relationships: VendorRelationships
 class VendorResponse(BaseModel):
+    meta: Optional[dict] = {}
+    data: Optional[list[VendorResourceObject]]
+    included: Optional[list[JSONAPIResourceObject]]
+    links: Optional[Pagination]
+
+## Vendor Info
+class VendorInfoAttributes(BaseModel):
+    category: str
+    content: str
+
+class VendorInfoRelationships(BaseModel):
+    vendor: JSONAPIRelationships
+
+class VendorInfoResourceObject(JSONAPIResourceIdentifier):
+    attributes: VendorInfoAttributes 
+    relationships: VendorInfoRelationships
+
+class VendorInfoResponse(BaseModel):
+    meta: Optional[dict] = {}
+    data: Optional[list[VendorInfoResourceObject]]
+    included: Optional[list[JSONAPIResourceObject]]
+    links: Optional[Pagination]
+
+class NewVendorInfoResourceObject(BaseModel):
+    type: str
+    attributes: VendorInfoAttributes
+    relationships: VendorInfoRelationships
+
+## Vendor Modifications
+class ExistingVendorInfo(NewVendorInfoResourceObject):
+    id: str|int
     ...
-class VendorDetailResponse(BaseModel):
-    ...
-class VendorQuery(BaseModel):
-    ...
-class NewVendorResourceObject(BaseModel):
-    ...
-class NewVendorDetailResourceObject(BaseModel):
-    ...
-class ExistingVendor(BaseModel):
-    ...
-class ExistingVendorDetail(BaseModel):
-    ...
+
+VendorQuery = create_model(
+    'VendorQuery',
+    **{field: (field_info.annotation, field_info) for field, field_info in Query.model_fields.items()},
+    **{f"fields_{field}":(Optional[str], None) for field in VendorRelationships.model_fields.keys()},
+    **{f"filter_{field}":(Optional[str], None) for field in VendorAttributes.model_fields.keys()},
+)
--- app/vendors/vendors.py
+++ app/vendors/vendors.py
@@ -4,59 +4,47 @@ from fastapi.routing import APIRouter
 from app import auth
 from app.vendors.models import (
     VendorResponse,
-    VendorDetailResponse,
+    VendorInfoResponse,
     VendorQuery,
-    NewVendorResourceObject,
-    NewVendorDetailResourceObject,
-    ExistingVendor,
-    ExistingVendorDetail
+    NewVendorInfoResourceObject,
+    ExistingVendorInfo
 )
 
 vendors = APIRouter(prefix='/vendors', tags=['vendors'])
 
 
 @vendors.get('', response_model=VendorResponse)
-async def vendors_collection(
+async def vendor_collection(
         query: VendorQuery=Depends(),
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> VendorResponse:
     raise HTTPException(status_code=501)
 
 @vendors.get('/{vendor_id}')
-async def vendor_detail(
+async def vendor(
         vendor_id: int,
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> VendorResponse:
     raise HTTPException(status_code=501)
 
-@vendors.delete('/{vendor_id}')
-async def delete_vendor(
+@vendors.post('/{vendor_id}/info')
+async def add_info(
+        body: NewVendorInfoResourceObject,
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> VendorInfoResponse:
+    raise HTTPException(status_code=501)
+
+@vendors.patch('/{vendor_id}/info')
+async def modify_info(
         vendor_id: int,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> None:
+        body: ExistingVendorInfo,
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> VendorInfoResponse:
     raise HTTPException(status_code=501)
 
-@vendors.post('')
-async def new_vendor(
-        body: NewVendorResourceObject,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> VendorResponse:
-    raise HTTPException(status_code=501)
-
-@vendors.post('/{vendor_id}/products')
-async def add_products(
-        body: NewVendorDetailResourceObject,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> VendorDetailResponse:
-    raise HTTPException(status_code=501)
-
-@vendors.patch('/{vendor_id}')
-async def modify_vendor(
+@vendors.delete('/{vendor_id}/info')
+async def delete_info(
         vendor_id: int,
-        body: ExistingVendor,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> VendorResponse:
-    raise HTTPException(status_code=501)
-
-@vendors.patch('/{vendor_id}/products')
-async def modify_products(
-        vendor_id: int,
-        body: ExistingVendorDetail,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)) -> VendorDetailResponse:
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> None:
     raise HTTPException(status_code=501)
