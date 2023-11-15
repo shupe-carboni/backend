d85f79ac7c562d2e7384032acad4d03f74b851b5
carboni-joseph <joe@carbonitech.com>
2023-11-15 14:23:09 -0500

    docstrings

--- app/relationships.py
+++ app/relationships.py
@@ -1,4 +1,6 @@
+"""Catch-all for undocumented links"""
 from fastapi import APIRouter, Depends, HTTPException
+from app.jsonapi import JSONAPIResourceObject
 from app import auth
 
 relationships = APIRouter()
@@ -9,8 +11,10 @@ def get_related_handler(
         id_: int,
         secondary: str,
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
-    ):
-    return HTTPException(status_code=501)
+    ) -> JSONAPIResourceObject:
+    """Returns a collection of resource objects representing the secondary resource
+        similar to GET /secondary filtering for the primary resource id_"""
+    raise HTTPException(status_code=501)
 
 @relationships.get("/{primary}/{id_}/relationships/{secondary}", tags=["relationships"])
 def get_self_relationship_handler(
@@ -19,4 +23,6 @@ def get_self_relationship_handler(
         secondary: str,
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ):
-    return HTTPException(status_code=501)
+    """Returns a list of Resource Identifiers: { "type": type, "id": id } related to id_
+        where the type is the seconady resource"""
+    raise HTTPException(status_code=501)
