48093dd63b87b5329060a48691888f7e101f3860
carboni-joseph <joe@carbonitech.com>
2023-11-16 10:22:39 -0500

    est base routes for customers & places

---
+++ app/customers/customers.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException, Depends
+from fastapi.routing import APIRouter
+from app import auth
+
+from app.customers.models import CustomerQuery, CustomerResponse
+
+customers = APIRouter(prefix='/customers', tags=['customers'])
+
+@customers.get('')
+async def customer_collection(
+        query: CustomerQuery=Depends(),
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> CustomerResponse:
+    raise HTTPException(status_code=501)
---
+++ app/places/places.py
@@ -0,0 +1,14 @@
+
+from fastapi import HTTPException, Depends
+from fastapi.routing import APIRouter
+from app import auth
+from app.places.models import PlaceQuery, PlaceResponse
+
+places = APIRouter(prefix='/places', tags=['places'])
+
+@places.get('')
+async def places_collection(
+        query: PlaceQuery=Depends(),
+        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
+    ) -> PlaceResponse:
+    raise HTTPException(status_code=501)
