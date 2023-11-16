1dfc190e4f7e9460cb4bae8978ec83137683e0db
carboni-joseph <joe@carbonitech.com>
2023-11-16 09:02:35 -0500

    include relationship routers

--- app/main.py
+++ app/main.py
@@ -4,13 +4,22 @@ from starlette.responses import RedirectResponse
 from app import relationships
 from app.quotes import quotes, quote_rel
 from app.vendors import vendors
+from app.customers import customer_rel
+from app.locations import location_rel
+from app.places import place_rel
+from app.products import product_rel
 
 app = FastAPI()
 
-app.include_router(relationships)
+## order matters
 app.include_router(vendors)
 quotes.include_router(quote_rel)
 app.include_router(quotes)
+app.include_router(customer_rel)
+app.include_router(location_rel)
+app.include_router(place_rel)
+app.include_router(product_rel)
+app.include_router(relationships)
 
 @app.get('/')
 async def home():
