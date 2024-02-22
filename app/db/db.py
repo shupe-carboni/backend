5c76b803e7123990fb6a9e9e59e44cc07522cf21
carboni-joseph <joe@carbonitech.com>
2024-02-22 12:31:43 -0500

    build in session factory, instantiate by db resource for importing

--- app/db/db.py
+++ app/db/db.py
@@ -4,6 +4,7 @@ import os
 from enum import StrEnum, auto
 from typing import Iterable, Any
 from sqlalchemy import create_engine, text, URL
+from sqlalchemy.orm import Session, sessionmaker
 from pandas import DataFrame, read_sql
 
 # NOTE `load_df` is too specific to the "adp" databse
@@ -31,39 +32,46 @@ class Database:
     # without doing this, the password is not properly passed due to url encoding
     # when passing the URL object
     _connection_str = _connection_url.render_as_string(hide_password=False)
-    ENGINE = create_engine(_connection_str)
+    _ENGINE = create_engine(_connection_str)
+    _SESSIONLOCAL = sessionmaker(bind=_ENGINE, autoflush=False, autocommit=False)
 
     def __init__(self, database_name: str='') -> None:
         self.PREFIX = database_name + "_" if database_name else None
-    
+
     def __str__(self) -> str:
-        return f"<Database obj, connection_path: {self._connection_str}, subgroup: {self.PREFIX[:-1]}>"
+        return f"<Database obj, connection_path: {self._connection_str}, subgroup: {self.PREFIX[:-1] if self.PREFIX else None}>"
+
+    def get_db(self):
+        session = self._SESSIONLOCAL()
+        try:
+            yield session
+        finally:
+            session.close()
     
     def full_table_name(self, table_name: str) -> str:
         return f"""{self.PREFIX}{table_name.replace('-', '_')}"""
     
-    def upload_df(self, data: DataFrame, table_name: str, primary_key: bool=True, if_exists: str='replace') -> None:
+    def upload_df(
+            self,
+            session: Session,
+            data: DataFrame,
+            table_name: str,
+            primary_key: bool=True,
+            if_exists: str='replace'
+        ) -> None:
         full_table_name = self.full_table_name(table_name)
-        data.to_sql(full_table_name, con=self.ENGINE, if_exists=if_exists, index=False)
+        data.to_sql(full_table_name, con=session.get_bind(), if_exists=if_exists, index=False)
         if primary_key and if_exists == 'replace':
-            with self.ENGINE.connect() as conn:
-                with conn as cur:
-                    try:
-                        sql = f"""
-                                ALTER TABLE {full_table_name}
-                                ADD COLUMN id SERIAL PRIMARY KEY;
-                        """
-                        cur.execute(text(sql))
-                    except Exception as e:
-                        print(e)
-                        conn.rollback()
-                    else:
-                        conn.commit()
-                    finally:
-                        conn.close()
+            with session.begin() as session:
+                sql = f"""
+                        ALTER TABLE {full_table_name}
+                        ADD COLUMN id SERIAL PRIMARY KEY;
+                """
+                session.execute(text(sql))
 
     def load_df(
             self,
+            session: Session,
             table_name: str,
             customer_id: int=None,
             is_null: str=None,
@@ -76,28 +84,23 @@ class Database:
         elif is_null:
             sql += f""" WHERE {is_null} IS NULL"""
         sql += ";"
-        return read_sql(sql, con=self.ENGINE, params=params)
+        return read_sql(sql, con=session.get_bind(), params=params)
     
-    def execute_and_commit(self, sql: str, params: Iterable[str|int|None]=None) -> Any:
+    def execute(
+            self,
+            session: Session,
+            sql: str,
+            params: Iterable[str|int|None]=None
+        ) -> Any:
         ## add prefix to custom query table_name
         substitution = lambda match: match.group(0).replace(match.group(1), f'{self.PREFIX}{match.group(1)}')
         sql = re.sub(r'(?:FROM|UPDATE|INSERT INTO|TABLE)\s+([^\s,;]+)', substitution, sql, count=1)
-        with self.ENGINE.connect() as conn:
-            try:
-                result = conn.execute(text(sql),parameters=params)
-            except:
-                import traceback as tb
-                print(tb.format_exc())
-                conn.rollback()
-                return None
-            else:
-                conn.commit()
-        return result
+        return session.execute(text(sql),params=params)
     
-    def test(self) -> str:
-        with self.ENGINE.connect() as conn:
-            with conn as cur:
-                result = cur.execute(text('SELECT version();'))
-                return result.fetchone()[0]
+    def test(self, session: Session) -> str:
+        with session.begin():
+            return session.execute(text('SELECT version();')).fetchone()[0]
 
 
+ADP_DB = Database('adp')
+SCA_DB = Database('sca')
