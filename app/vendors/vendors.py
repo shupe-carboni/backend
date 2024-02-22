2aac2a0898d90d5595168a5bbe86125fbbb5d6f0
carboni-joseph <joe@carbonitech.com>
2024-02-22 12:33:11 -0500

    use token helper and annoted type

--- app/vendors/vendors.py
+++ app/vendors/vendors.py
@@ -1,4 +1,4 @@
-
+from typing import Annotated
 from fastapi import HTTPException, Depends
 from fastapi.routing import APIRouter
 from app import auth
@@ -12,39 +12,40 @@ from app.vendors.models import (
 
 vendors = APIRouter(prefix='/vendors', tags=['vendors'])
 
+VendorsPerm = Annotated[auth.VerifiedToken, Depends(auth.vendor_perms_present)]
 
 @vendors.get('', response_model=VendorResponse)
 async def vendor_collection(
+        token: VendorsPerm,
         query: VendorQuery=Depends(), # type: ignore
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> VendorResponse:
     raise HTTPException(status_code=501)
 
 @vendors.get('/{vendor_id}')
 async def vendor(
+        token: VendorsPerm,
         vendor_id: int,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> VendorResponse:
     raise HTTPException(status_code=501)
 
 @vendors.post('/{vendor_id}/info')
 async def add_info(
+        token: VendorsPerm,
         body: NewVendorInfoResourceObject,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> VendorInfoResponse:
     raise HTTPException(status_code=501)
 
 @vendors.patch('/{vendor_id}/info')
 async def modify_info(
+        token: VendorsPerm,
         vendor_id: int,
         body: ExistingVendorInfo,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> VendorInfoResponse:
     raise HTTPException(status_code=501)
 
 @vendors.delete('/{vendor_id}/info')
 async def delete_info(
+        token: VendorsPerm,
         vendor_id: int,
-        token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> None:
     raise HTTPException(status_code=501)
