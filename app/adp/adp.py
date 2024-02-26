f0ff015065980e75f1e346e0fa2790afb9306034
carboni-joseph <joe@carbonitech.com>
2024-02-26 17:53:13 -0500

    add ability to add ratings from a file

--- app/adp/adp.py
+++ app/adp/adp.py
@@ -1,13 +1,16 @@
 from typing import Literal, Optional, Mapping, Any, Annotated
-from fastapi import HTTPException, Depends, status
+from fastapi import HTTPException, Depends, status, UploadFile
 from starlette.background import BackgroundTask
 from fastapi.responses import StreamingResponse
 from fastapi.routing import APIRouter
+from pandas import read_excel, read_csv
+from numpy import nan
 
 from app import auth
 from app.db import Session, ADP_DB, Stage
-from app.adp.main import generate_program, add_model_to_program
-from app.adp.models import CoilProgQuery, CoilProgResp, NewCoilRObj, NewAHRObj
+from app.adp.main import generate_program, add_model_to_program, add_parts_to_program, add_ratings_to_program
+from app.adp.programs import EmptyProgram
+from app.adp.models import CoilProgQuery, CoilProgResp, NewCoilRObj, NewAHRObj, Rating, Ratings
 
 adp = APIRouter(prefix='/adp', tags=['adp'])
 class XLSXFileResponse(StreamingResponse):
@@ -59,8 +62,14 @@ def customer_program(
     """Generate a program excel file and return for download"""
     if one_time_hash:
         stage_str = stage.upper()
-        file = generate_program(session=session, customer_id=adp_customer_id, stage=stage_str)
-        return XLSXFileResponse(content=file.file_data, filename=file.file_name)
+        try:
+            file = generate_program(session=session, customer_id=adp_customer_id, stage=stage_str)
+            return XLSXFileResponse(content=file.file_data, filename=file.file_name)
+        except EmptyProgram:
+            raise HTTPException(status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
     else:
         print("Enforce that the customer is allowed to have the program associated with the adp_customer_id selected")
         raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
@@ -68,9 +77,9 @@ def customer_program(
 @adp.post('/{adp_customer_id}/program/coils')
 def add_to_coil_program(
         token: ADPPerm,
+        session: NewSession,
         adp_customer_id: int,
         new_coil: NewCoilRObj,
-        session: NewSession
         ):
     """add coil product for a customer"""
     if token.permissions.get('adp') >= auth.ADPPermPriority.sca_employee:
@@ -83,9 +92,9 @@ def add_to_coil_program(
 @adp.post('/{adp_customer_id}/program/air-handlers')
 def add_to_ah_program(
         token: ADPPerm,
+        session: NewSession,
         adp_customer_id: int,
         new_ah: NewAHRObj,
-        session: NewSession
     ):
     """modify product listed on an existing program"""
     if token.permissions.get('adp') >= auth.ADPPermPriority.sca_employee:
@@ -96,10 +105,26 @@ def add_to_ah_program(
         raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
 
 @adp.post('/{adp_customer_id}/program/ratings')
-def add_to_ah_program(
+async def add_program_ratings(
         token: ADPPerm,
-        ratings: None,
+        ratings_file: UploadFile,
+        adp_customer_id: int,
         session: NewSession
     ):
-    """modify product listed on an existing program"""
-    raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
+    """add ratings to a customer's program"""
+    if token.permissions.get('adp') >= auth.ADPPermPriority.sca_employee:
+        ratings_data = await ratings_file.read()
+        match ratings_file.content_type:
+            case 'text/csv':
+                ratings_df = read_csv(ratings_data).replace({nan: None})
+            case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                ratings_df = read_excel(ratings_data).replace({nan: None})
+            case _:
+                raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        
+        ratings = Ratings(ratings=[Rating(**row._asdict()) for row in ratings_df.itertuples()])
+        add_ratings_to_program(session=session, adp_customer_id=adp_customer_id, ratings=ratings)
+
+    else:
+        print("Enforce that the customer is allowed to add ratings to the adp_customer_id selected")
+        raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
