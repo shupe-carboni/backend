c0901ab495abda34fb441b80efa867ff57d17751
carboni-joseph <joe@carbonitech.com>
2024-02-29 12:05:57 -0500

    updated to use customer id directly

--- app/adp/extraction/parts.py
+++ app/adp/extraction/parts.py
@@ -1,14 +1,15 @@
 from app.db import ADP_DB, Session
 from pandas import DataFrame
 
-def add_parts_to_program(session: Session, adp_alias: str, part_nums: list[str]) -> None:
-    customers = ADP_DB.load_df(session, 'customers')
-    customer_id = customers.loc[customers['adp_alias'] == adp_alias,'id'].item() if adp_alias in customers['adp_alias'].values else None
-    if not customer_id:
-        raise Exception('customer not found')
+def add_parts_to_program(session: Session, adp_customer_id: int, part_nums: list[str]) -> None:
     df = DataFrame(part_nums, columns=['part_number'])
-    df.insert(0, 'customer_id', customer_id)
+    df.insert(0, 'customer_id', adp_customer_id)
     with session.begin():
-        ADP_DB.upload_df(session=session, data=df, table_name='program_parts', if_exists='append')
+        ADP_DB.upload_df(
+            session=session,
+            data=df,
+            table_name='program_parts',
+            primary_key=False,
+            if_exists='append')
 
     
