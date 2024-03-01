7ec82aa77575e60ba0c23c15f8e63adb3d2532db
carboni-joseph <joe@carbonitech.com>
2024-03-01 16:47:18 -0500

    add imports for adp quotes

--- app/adp/__init__.py
+++ app/adp/__init__.py
@@ -1,2 +1,4 @@
 from app.adp.routes import adp
+from app.adp.quotes.quotes import quotes as adp_quotes
+from app.adp.quotes.job_quotes.relationships import quote_rel as adp_quote_rel
 import app.adp.main as api
