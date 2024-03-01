086bcb01cc1757d0227fd766d647f3c0066049e9
carboni-joseph <joe@carbonitech.com>
2024-03-01 16:45:42 -0500

    fix imports and naming

--- app/places/relationships.py
+++ app/places/relationships.py
@@ -2,33 +2,33 @@ from fastapi import HTTPException, Depends
 from fastapi.routing import APIRouter
 from app import auth
 
-from app.locations.models import RelatedLocationResponse, LocationRelationshipsResponse
-from app.quotes.job_quotes.models import RelatedQuoteResponse, QuoteRelationshipsResponse
+from app.customers.locations.models import RelatedLocationResponse, LocationRelationshipsResponse
+from app.adp.quotes.job_quotes.models import RelatedQuoteResponse, QuoteRelationshipsResponse
 
 place_rel = APIRouter(tags=['places'])
 
-@place_rel.get('/{place_id}/locations')
+@place_rel.get('/{place_id}/customer-locations')
 async def related_location(
         place_id: int,
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> RelatedLocationResponse:
     raise HTTPException(status_code=501)
 
-@place_rel.get('/{place_id}/relationships/locations')
+@place_rel.get('/{place_id}/relationships/customer-locations')
 async def place_location_relationships(
         place_id: int,
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> LocationRelationshipsResponse:
     raise HTTPException(status_code=501)
 
-@place_rel.get('/{place_id}/quotes')
+@place_rel.get('/{place_id}/adp-quotes')
 async def related_quotes(
         place_id: int,
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
     ) -> RelatedQuoteResponse:
     raise HTTPException(status_code=501)
 
-@place_rel.get('/{place_id}/relationships/quotes')
+@place_rel.get('/{place_id}/relationships/adp-quotes')
 async def place_quotes_relationships(
         place_id: int,
         token: auth.VerifiedToken = Depends(auth.authenticate_auth0_token)
