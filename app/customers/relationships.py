d605f4b72f6affd860df0b4dcd3dabd9eb5d8bd8
carboni-joseph <joe@carbonitech.com>
2024-03-05 15:54:07 -0500

    adjust names and try get_related

--- app/customers/relationships.py
+++ app/customers/relationships.py
@@ -2,24 +2,31 @@
 from typing import Annotated
 from fastapi import HTTPException, Depends
 from fastapi.routing import APIRouter
-from app import auth
+from sqlalchemy.orm import Session
 
+from app import auth
+from app.db import SCA_DB
 from app.customers.locations.models import RelatedLocationResponse, LocationRelationshipsResponse
 from app.adp.models import CustomersResp
+from app.jsonapi.sqla_models import serializer
 
+api_type = 'customers'
 customer_rel = APIRouter(tags=['customers'])
 CustomersPerm = Annotated[auth.VerifiedToken, Depends(auth.customers_perms_present)]
 ADPPerm = Annotated[auth.VerifiedToken, Depends(auth.adp_perms_present)]
+NewSession = Annotated[Session, Depends(SCA_DB.get_db)]
 
-@customer_rel.get('/{customer_id}/locations')
+@customer_rel.get('/{customer_id}/customer-locations', response_model=RelatedLocationResponse, response_model_exclude_none=True)
 async def related_location(
+        session: NewSession,
         customer_id: int,
         token: CustomersPerm,
     ) -> RelatedLocationResponse:
-    raise HTTPException(status_code=501)
+    return serializer.get_related(session=session, query={}, api_type=api_type, obj_id=customer_id, rel_key='customer-locations')
 
-@customer_rel.get('/{customer_id}/relationships/locations')
+@customer_rel.get('/{customer_id}/relationships/customer-locations')
 async def customer_location_relationships(
+        session: NewSession,
         customer_id: int,
         token: CustomersPerm,
     ) -> LocationRelationshipsResponse:
@@ -27,6 +34,7 @@ async def customer_location_relationships(
 
 @customer_rel.get('/{customer_id}/adp-customers')
 async def related_location(
+        session: NewSession,
         customer_id: int,
         token: ADPPerm,
     ) -> CustomersResp:
@@ -34,6 +42,7 @@ async def related_location(
 
 @customer_rel.get('/{customer_id}/relationships/adp-customers')
 async def customer_location_relationships(
+        session: NewSession,
         customer_id: int,
         token: ADPPerm,
     ) -> LocationRelationshipsResponse:
@@ -41,6 +50,7 @@ async def customer_location_relationships(
 
 @customer_rel.get('/{customer_id}/adp-customer-terms')
 async def related_location(
+        session: NewSession,
         customer_id: int,
         token: ADPPerm,
     ) -> RelatedLocationResponse:
@@ -48,6 +58,7 @@ async def related_location(
 
 @customer_rel.get('/{customer_id}/relationships/adp-customer-terms')
 async def customer_location_relationships(
+        session: NewSession,
         customer_id: int,
         token: ADPPerm,
     ) -> LocationRelationshipsResponse:
