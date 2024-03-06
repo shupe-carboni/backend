6b4925dfe0731b06458f036bd3125ca0da036322
carboni-joseph <joe@carbonitech.com>
2024-03-06 09:06:44 -0500

    override test tokens moved and expanded to capture all current permission types

---
+++ tests/auth_overrides.py
@@ -0,0 +1,44 @@
+from functools import partial
+from app.auth import Permissions, perm_category_present
+
+class AdminToken:
+    permissions = {perm.name: perm.value.sca_admin for perm in Permissions}
+    def perm_level(category):
+        return Permissions[category].value.sca_admin.value
+
+class SCAEmployeeToken:
+    permissions = {perm.name: perm.value.sca_employee for perm in Permissions}
+    def perm_level(category):
+        return Permissions[category].value.sca_employee.value
+
+class CustomerAdminToken:
+    permissions = {perm.name: perm.value.customer_admin for perm in Permissions}
+    def perm_level(category):
+        return Permissions[category].value.customer_admin.value
+
+class CustomerManagerToken:
+    permissions = {perm.name: perm.value.customer_manager for perm in Permissions}
+    def perm_level(category):
+        return Permissions[category].value.customer_manager.value
+
+class CustomerStandardToken:
+    permissions = {perm.name: perm.value.customer_std for perm in Permissions}
+    def perm_level(category):
+        return Permissions[category].value.customer_std.value
+
+def auth_as_sca_admin(perm_category):
+    return partial(perm_category_present, AdminToken, perm_category)
+
+def auth_as_sca_employee(perm_category):
+    return partial(perm_category_present, SCAEmployeeToken, perm_category)
+
+def auth_as_customer_admin(perm_category):
+    return partial(perm_category_present, CustomerAdminToken, perm_category)
+
+def auth_as_customer_manager(perm_category):
+    return partial(perm_category_present, CustomerManagerToken, perm_category)
+
+def auth_as_customer_std(perm_category):
+    return partial(perm_category_present, CustomerStandardToken, perm_category)
+
+
