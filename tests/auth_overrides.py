e1f2c5c5074380cb1fd89cae539410d70bc351c7
carboni-joseph <joe@carbonitech.com>
2024-03-07 17:22:32 -0500

    add `email_verified = True` to all mock tokens and use my own email address for a user email in `CustomerStandardToken` (may need to do this to all tokens for testing)

--- tests/auth_overrides.py
+++ tests/auth_overrides.py
@@ -1,28 +1,36 @@
 from functools import partial
 from app.auth import Permissions, perm_category_present
+from dotenv import load_dotenv; load_dotenv()
+from os import getenv
 
 class AdminToken:
     permissions = {perm.name: perm.value.sca_admin for perm in Permissions}
+    email_verified = True
     def perm_level(category):
         return Permissions[category].value.sca_admin.value
 
 class SCAEmployeeToken:
     permissions = {perm.name: perm.value.sca_employee for perm in Permissions}
+    email_verified = True
     def perm_level(category):
         return Permissions[category].value.sca_employee.value
 
 class CustomerAdminToken:
     permissions = {perm.name: perm.value.customer_admin for perm in Permissions}
+    email_verified = True
     def perm_level(category):
         return Permissions[category].value.customer_admin.value
 
 class CustomerManagerToken:
     permissions = {perm.name: perm.value.customer_manager for perm in Permissions}
+    email_verified = True
     def perm_level(category):
         return Permissions[category].value.customer_manager.value
 
 class CustomerStandardToken:
     permissions = {perm.name: perm.value.customer_std for perm in Permissions}
+    email_verified = True
+    email = getenv('ADMIN_EMAIL')
     def perm_level(category):
         return Permissions[category].value.customer_std.value
 
