760b747168e7919adf5d1bd1583018cb215afc36
carboni-joseph <joe@carbonitech.com>
2024-03-08 13:14:15 -0500

    add more tests for getting coil programs and specific products. understanding that resources by id are specific products. customer id is implicitly selected through filtering

--- tests/test_adp.py
+++ tests/test_adp.py
@@ -9,24 +9,15 @@ from tests import auth_overrides
 # BUG should I really be using just ADP permissions for the adp relationships?
 
 test_client = TestClient(app)
-CUSTOMER_ID = 13 # NOTE should or can I randomize this?
+# NOTE should or can I randomize this?
+# this customer id is not explitly used. The filtering process behind the
+# get_collection and get_resource implementations already selects for the 
+# proper adp_customer_id values based on a mapping table
+ADP_CUSTOMER_ID = 11
+VALID_PRODUCT_ID = 84
+INVALID_PRODUCT_ID = 1 # invalid for the customer but not SCA
 
-## SCA ADMIN
-def test_customer_collection_as_sca_admin():
-    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_sca_admin('adp')
-    response = test_client.get('/adp/adp-coil-programs')
-    assert response.status_code == 200
-    assert CoilProgResp(**response.json())
-    app.dependency_overrides[adp_perms_present] = {}
-
-def test_customer_collection_as_customer_std():
-    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_customer_std('adp')
-    response = test_client.get('/adp/adp-coil-programs')
-    assert response.status_code == 200
-    assert CoilProgResp(**response.json())
-    app.dependency_overrides[adp_perms_present] = {}
-
-def test_customer_collection_filtering():
+def test_customer_coil_program_collection_filtering():
     """this is dependent on the data - in that we expect more than one customer to have data to return"""
     ## coil programs is the example
     ## I care only about the id numbers in "included", no other data
@@ -39,3 +30,93 @@ def test_customer_collection_filtering():
     app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_sca_admin('adp')
     response = test_client.get(trimmed_data_req)
     assert len(set(map(lambda x: x['id'], response.json()['included']))) > 1
+
+## SCA ADMIN
+def test_customer_coil_program_collection_as_sca_admin():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_sca_admin('adp')
+    response = test_client.get('/adp/adp-coil-programs')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    app.dependency_overrides[adp_perms_present] = {}
+
+def test_customer_coil_program_resource_as_sca_admin():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_sca_admin('adp')
+    response = test_client.get(f'/adp/adp-coil-programs/{VALID_PRODUCT_ID}')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    response = test_client.get(f'/adp/adp-coil-programs/{INVALID_PRODUCT_ID}')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    app.dependency_overrides[adp_perms_present] = {}
+
+## SCA EMPLOYEE
+def test_customer_coil_program_collection_as_sca_employee():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_sca_employee('adp')
+    response = test_client.get('/adp/adp-coil-programs')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    app.dependency_overrides[adp_perms_present] = {}
+
+def test_customer_coil_program_resource_as_sca_employee():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_sca_employee('adp')
+    response = test_client.get(f'/adp/adp-coil-programs/{VALID_PRODUCT_ID}')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    response = test_client.get(f'/adp/adp-coil-programs/{INVALID_PRODUCT_ID}')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    app.dependency_overrides[adp_perms_present] = {}
+
+## CUSTOMER ADMIN
+def test_customer_coil_program_collection_as_customer_admin():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_customer_admin('adp')
+    response = test_client.get('/adp/adp-coil-programs')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    app.dependency_overrides[adp_perms_present] = {}
+
+def test_customer_coil_program_resource_as_customer_admin():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_customer_admin('adp')
+    response = test_client.get(f'/adp/adp-coil-programs/{VALID_PRODUCT_ID}')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    response = test_client.get(f'/adp/adp-coil-programs/{INVALID_PRODUCT_ID}')
+    assert response.status_code == 204
+    assert response.content == str('').encode()
+    app.dependency_overrides[adp_perms_present] = {}
+
+## CUSTOMER MANAGER
+def test_customer_coil_program_collection_as_customer_manager():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_customer_manager('adp')
+    response = test_client.get('/adp/adp-coil-programs')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    app.dependency_overrides[adp_perms_present] = {}
+
+def test_customer_coil_program_resource_as_customer_manager():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_customer_manager('adp')
+    response = test_client.get(f'/adp/adp-coil-programs/{VALID_PRODUCT_ID}')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    response = test_client.get(f'/adp/adp-coil-programs/{INVALID_PRODUCT_ID}')
+    assert response.status_code == 204
+    assert response.content == str('').encode()
+    app.dependency_overrides[adp_perms_present] = {}
+
+## CUSTOMER STANDARD
+def test_customer_coil_program_collection_as_customer_std():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_customer_std('adp')
+    response = test_client.get('/adp/adp-coil-programs')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    app.dependency_overrides[adp_perms_present] = {}
+
+def test_customer_coil_program_resource_as_customer_std():
+    app.dependency_overrides[adp_perms_present] = auth_overrides.auth_as_customer_std('adp')
+    response = test_client.get(f'/adp/adp-coil-programs/{VALID_PRODUCT_ID}')
+    assert response.status_code == 200
+    assert CoilProgResp(**response.json())
+    response = test_client.get(f'/adp/adp-coil-programs/{INVALID_PRODUCT_ID}')
+    assert response.status_code == 204
+    assert response.content == str('').encode()
+    app.dependency_overrides[adp_perms_present] = {}
