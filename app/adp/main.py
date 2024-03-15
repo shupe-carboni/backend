f4df62d8e049dfe5a2a4e9339770c80b9352cce5
carboni-joseph <joe@carbonitech.com>
2024-03-15 11:33:53 -0400

    bubble up parsing modes Enum

--- app/adp/main.py
+++ app/adp/main.py
@@ -1,6 +1,6 @@
 from app.adp.utils import prune_ratings
 from app.adp.utils.workbook_factory import generate_program
-from app.adp.extraction.models import  add_model_to_program, parse_model_string
+from app.adp.extraction.models import  add_model_to_program, parse_model_string, ParsingModes
 from app.adp.extraction.ratings import (
     update_ratings_reference,
     add_ratings_to_program,
