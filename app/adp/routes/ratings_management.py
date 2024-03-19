1b7b9d88d1e3cf564a5567d97bd952530d614e2b
carboni-joseph <joe@carbonitech.com>
2024-03-19 16:41:07 -0400

    run ref update in thread

--- app/adp/routes/ratings_management.py
+++ app/adp/routes/ratings_management.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Annotated
+from concurrent.futures import ThreadPoolExecutor
 from fastapi import HTTPException, Depends, status, BackgroundTasks, Response
 from fastapi.routing import APIRouter
 
@@ -12,18 +13,21 @@ logger = logging.getLogger('uvicorn.info')
 ADPPerm = Annotated[auth.VerifiedToken, Depends(auth.adp_perms_present)]
 NewSession = Annotated[Session, Depends(ADP_DB.get_db)]
 
+executor = ThreadPoolExecutor(max_workers=2)
+
 @ratings_admin.get('/update-ratings-reference')
 def update_ratings_ref(
         token: ADPPerm,
         session: NewSession,
-        bg: BackgroundTasks
+        # bg: BackgroundTasks
     ):
     """Update the rating reference table in the background
         Due to the size of the table being downloaded, this
         is a long-running task"""
     if token.permissions.get('adp') >= auth.ADPPermPriority.sca_admin:
         logger.info('Update request received for ADP Ratings Reference Table. Sending to background')
-        bg.add_task(update_ratings_reference, session=session)
+        executor.submit(update_ratings_reference, session=session)
+        # bg.add_task(update_ratings_reference, session=session)
         return Response(status_code=status.HTTP_202_ACCEPTED)
     else:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
