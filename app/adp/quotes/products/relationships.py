b951f8ee5ffd3531b415d30f2787fcf4a0e37ee7
carboni-joseph <joe@carbonitech.com>
2024-04-04 17:37:36 -0400

    path correction

--- app/adp/quotes/products/relationships.py
+++ app/adp/quotes/products/relationships.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException, Depends
 from fastapi.routing import APIRouter
 from app import auth
-from app.quotes.job_quotes.models import RelatedQuoteResponse, QuoteRelationshipsResponse
+from app.adp.quotes.job_quotes.models import RelatedQuoteResponse, QuoteRelationshipsResponse
 
 product_rel = APIRouter(prefix='/products', tags=['products'])
 
