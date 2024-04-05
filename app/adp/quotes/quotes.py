cafb12ddbb7edb0c770738533f6982af5afe61d7
carboni-joseph <joe@carbonitech.com>
2024-04-05 15:36:19 -0400

    incomplete implementation of the post quote operation

--- app/adp/quotes/quotes.py
+++ app/adp/quotes/quotes.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from typing import Annotated
+from typing import Annotated, Optional
 from dataclasses import dataclass
 from datetime import datetime
 from sqlalchemy.orm import Session
@@ -15,6 +15,7 @@ from app.adp.quotes.job_quotes.models import (
 from app.jsonapi.sqla_models import ADPQuote
 
 QUOTES_RESOURCE = ADPQuote.__jsonapi_type_override__
+S3_DIR = '/adp/quotes/'
 quotes = APIRouter(prefix=f'/{QUOTES_RESOURCE}', tags=['adp quotes'])
 
 ADPQuotesPerm = Annotated[auth.VerifiedToken, Depends(auth.adp_quotes_perms)]
@@ -26,7 +27,8 @@ class NewFile:
     file_name: str
     file_mime: str
     file_content: bytes|BytesIO
-
+    def __post_init__(self) -> None:
+        self.s3 = S3_DIR
 
 @quotes.get('', response_model=QuoteResponse, response_model_exclude_none=True)
 async def quote_collection(
@@ -70,31 +72,76 @@ async def one_quote(
 async def new_quote(
         token: ADPQuotesPerm,
         session: NewSession,
-        quote_doc: UploadFile,
-        plans_doc: UploadFile,
         adp_quote_id: str = Form(defualt=None),
         job_name: str = Form(),
         expires_at: datetime = Form(),
         status: Stage = Form(),
+        quote_doc: Optional[UploadFile]=None,
+        plans_doc: Optional[UploadFile]=None,
         place_id: int = Form(),
         customer_location_id: int = Form(),
         adp_customer_id: int = Form()
     ) -> QuoteResponse:
     """Create a new quote.
-        - Quote ID, quote documents and plan documents are not required for creation.
-        - Create timestamp ought to generated client-side and sent with the request, whereas
+        - ADP Quote ID, quote documents and plan documents are not required for creation.
+        - ADP Quote ID comes from ADP and ought to be added in later if not supplied on creation.
+        - Create timestamp is auto-generated and stuffed into the JSONAPI request
         - Expiration date is determined by rules associated with quote creation (i.e. +90 days).
         - Stage should defualt to 'proposed', but ought to be selectable."""
     # TODO generate filenames and upload documents to S3. Plug the links to them into a NewQuote before post_collection
     created_at: datetime = datetime.today().date()
     if token.permissions.get('quotes') >= auth.QuotePermPriority.sca_employee:
-        # do it
+        attrs = {
+            'job_name': job_name,
+            'created_at': created_at,
+            'expires_at': expires_at,
+            'staus': status,
+        }
+        if adp_quote_id:
+            attrs['adp_quote_id'] = adp_quote_id
+        if quote_doc:
+            quote_file = NewFile(
+                file_name=S3_DIR+quote_doc.filename,
+                file_mime=quote_doc.content_type,
+                file_content=await quote_doc.read()
+            )
+            # TODO do the upload to AWS
+            attrs['quote_doc'] = quote_file.s3
+
+        if plans_doc:
+            plans_file = NewFile(
+                file_name=S3_DIR+plans_doc.filename,
+                file_mime=plans_doc.content_type,
+                file_content=await plans_doc.read()
+            )
+            # TODO do the upload to AWS
+            attrs['plans_doc'] = plans_file.s3
+
         new_quote_obj = {
             "type": QUOTES_RESOURCE,
-            "attributes": {
-                ''
+            "attributes": attrs,
+            "relationships": {
+                'places': {
+                    'data': {
+                        'id': place_id,
+                        'type': 'places'
+                    }
+                },
+                'customer-locations': {
+                    'data': {
+                        'id': customer_location_id,
+                        'type': 'customer-locations'
+                    }
+                },
+                'adp-customers': {
+                    'data': {
+                        'id': adp_customer_id,
+                        'type': 'adp-customers'
+                    }
+                },
             }
         }
+        new_quote = NewQuote(data=new_quote_obj)
         raise HTTPException(status_code=501)
     raise HTTPException(status_code=401)
 
