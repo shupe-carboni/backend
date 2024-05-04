f2cf4cf1b7d30767d0e0ac5a64a56dd6290ba535
carboni-joseph <joe@carbonitech.com>
2024-05-04 15:10:30 -0400

    stange endpoints and add CMMSSNS auth token storage & retrieval

--- app/customers/customers.py
+++ app/customers/customers.py
@@ -1,10 +1,17 @@
 from typing import Annotated
-from fastapi import Depends
+from os import getenv
+from time import sleep
+from dataclasses import dataclass
+from requests import get, post, patch, delete
+from fastapi import Depends, HTTPException, status
 from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 
 from app import auth
-from app.customers.models import CustomerQuery, CustomerResponse, CustomerQueryJSONAPI
+from app.customers.models import (
+    CustomerQuery, CustomerResponse, CustomerQueryJSONAPI,
+    NewCustomer, ModCustomer
+)
 from app.db.db import SCA_DB
 from app.jsonapi.sqla_models import SCACustomer
 from app.jsonapi.core_models import convert_query
@@ -12,11 +19,52 @@ from app.jsonapi.core_models import convert_query
 API_TYPE = SCACustomer.__jsonapi_type_override__
 customers = APIRouter(prefix=f'/{API_TYPE}', tags=['customers'])
 
-CustomersPerm = Annotated[auth.VerifiedToken, Depends(auth.customers_perms_present)]
+CMMSSNS_URL: str = getenv('CMMSSNS_AUDIENCE')
+
+CustomersPerm = Annotated[
+    auth.VerifiedToken,
+    Depends(auth.customers_perms_present)
+]
 NewSession = Annotated[Session, Depends(SCA_DB.get_db)]
 converter = convert_query(CustomerQueryJSONAPI)
 
-@customers.get('', response_model=CustomerResponse, response_model_exclude_none=True)
+@dataclass
+class CMMSSNSToken:
+    value: str
+
+    @classmethod
+    def get_token(cls, retry: int=0) -> None:
+        if retry > 2:
+            raise Exception('Unable to obtain auth token from CMMSSNS API')
+        CMMSSNS_AUTH0 = getenv('CMMSSNS_AUTH0_DOMAIN')
+        payload = {
+            'client_id': getenv('CMMSSNS_CLIENT_ID'),
+            'client_secret': getenv('CMMSSNS_CLIENT_SECRET'),
+            'audience': CMMSSNS_URL,
+            'grant_type': getenv('CMMSSNS_GRANT_TYPE')
+        }
+        resp = post(CMMSSNS_AUTH0+'/oauth/token', json=payload)
+        if resp.status_code == 200:
+            resp_body = resp.json()
+            token_type = resp_body['token_type']
+            access_token = resp_body['access_token']
+            cls.value = f"{token_type} {access_token}"
+        else:
+            retry += 1
+            cls.get_token(retry=retry)
+    
+    @classmethod
+    def auth_header(cls) -> dict[str,str]:
+        if not cls.value:
+            cls.get_token()
+        return {'Authorization': cls.value}
+
+@customers.get(
+        '',
+        response_model=CustomerResponse,
+        response_model_exclude_none=True,
+        tags=['jsonapi']
+    )
 async def customer_collection(
         session: NewSession,
         token: CustomersPerm,
@@ -32,7 +80,12 @@ async def customer_collection(
         query=converter(query)
     )
 
-@customers.get('/{customer_id}', response_model=CustomerResponse, response_model_exclude_none=True)
+@customers.get(
+        '/{customer_id}',
+        response_model=CustomerResponse,
+        response_model_exclude_none=True,
+        tags=['jsonapi']
+    )
 async def customer(
         session: NewSession,
         token: CustomersPerm,
@@ -48,3 +101,54 @@ async def customer(
         query=converter(query),
         obj_id=customer_id
     )
+
+# TODO
+"""
+    1. when a customer is added, call out to the CMMSSNS API to either 
+        send back or generate a customer id.
+    2. When creating a new customer loction, POST that to the CMMSSNS
+        application.
+        - if CMMSSNS creates a new branch, should it be sent down or
+        is it on this application to check for it first?
+    3. For each applicable vendor, make a record in respective
+        customer tables for those vendors as well as mapping tables to 
+        branches (at least one).
+    4. Add ancillary required records (i.e. customer payment tems, 
+      logo file, special prepaid frieght terms, etc.)
+    5. If files are required (i.e. images), upload them.
+"""
+@customers.post(
+        '',
+        response_model=CustomerResponse,
+        response_model_exclude_none=True,
+        tags=['jsonapi']
+    )
+async def new_customer(
+        session: NewSession,
+        token: CustomersPerm,
+        new_customer: NewCustomer
+    ) -> CustomerResponse:
+    raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
+
+
+@customers.patch(
+        '/{customer_id}',
+        response_model=CustomerResponse,
+        response_model_exclude_none=True,
+        tags=['jsonapi']
+    )
+async def mod_customer(
+        session: NewSession,
+        token: CustomersPerm,
+        customer_id: int,
+        mod_customer: ModCustomer
+    ) -> CustomerResponse:
+    raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
+
+@customers.delete('/{customer_id}',tags=['jsonapi'])
+async def del_customer(
+        session: NewSession,
+        token: CustomersPerm,
+        customer_id: int
+    ) -> None:
+    raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
