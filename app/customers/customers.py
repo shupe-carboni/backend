0339101fe6b1e92e661d487e1fd39dc484455f2f
carboni-joseph <joe@carbonitech.com>
2024-05-09 13:50:55 -0400

    added retry sleep time, impl customer search on CMMSSNS service, impl logo file upload to S3

--- app/customers/customers.py
+++ app/customers/customers.py
@@ -3,17 +3,17 @@ from os import getenv
 from time import sleep
 from dataclasses import dataclass
 from requests import get, post, patch, delete
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, UploadFile
 from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 
 from app import auth
 from app.customers.models import (
     CustomerQuery, CustomerResponse, CustomerQueryJSONAPI,
-    NewCustomer, ModCustomer
+    NewCustomer, ModCustomer, CMMSSNSCustomers
 )
-from app.db.db import SCA_DB
-from app.jsonapi.sqla_models import SCACustomer
+from app.db.db import SCA_DB, S3, File
+from app.jsonapi.sqla_models import SCACustomer, serializer
 from app.jsonapi.core_models import convert_query
 
 API_TYPE = SCACustomer.__jsonapi_type_override__
@@ -30,12 +30,14 @@ converter = convert_query(CustomerQueryJSONAPI)
 
 @dataclass
 class CMMSSNSToken:
-    value: str
+    value: str=''
 
     @classmethod
     def get_token(cls, retry: int=0) -> None:
         if retry > 2:
             raise Exception('Unable to obtain auth token from CMMSSNS API')
+        if retry:
+            sleep(retry)
         CMMSSNS_AUTH0 = getenv('CMMSSNS_AUTH0_DOMAIN')
         payload = {
             'client_id': getenv('CMMSSNS_CLIENT_ID'),
@@ -81,6 +83,34 @@ async def customer_collection(
     )
 
 @customers.get(
+        '/cmmssns-customers',
+        response_model=CMMSSNSCustomers
+    )
+async def cmmssns_customers(
+        token: CustomersPerm,
+        search_term: str
+    ) -> CMMSSNSCustomers:
+    """
+        Special endpoint for querying a protected endpoint on 
+        another service. The intent is to allow the user to perform a 
+        fuzzy search for customer entities on the CMMSSNS service 
+        in order to avoid potential duplication.
+    """
+    # NOTE if this path comes after the get-resource path below, 
+    # that one will take over and return a typing error
+    customer_perm = token.permissions.get('customers')
+    authorized = customer_perm >= auth.CustomersPermPriority.sca_employee
+    if authorized:
+        customers = CMMSSNS_URL + 'customers'
+        q_filter = f'filter={search_term.upper().strip()}'
+        url = customers + f'?{q_filter}&postgres_fuzzy_match=true'
+        resp = get(url=url, headers=CMMSSNSToken.auth_header())
+        if resp.status_code != 200:
+            raise HTTPException(resp.status_code, resp.text)
+        return CMMSSNSCustomers(data=resp.json())
+    return HTTPException(status.HTTP_401_UNAUTHORIZED)
+
+@customers.get(
         '/{customer_id}',
         response_model=CustomerResponse,
         response_model_exclude_none=True,
@@ -105,7 +135,7 @@ async def customer(
 # TODO
 """
     1. when a customer is added, call out to the CMMSSNS API to either 
-        send back or generate a customer id.
+        send back or generate a customer id. (HALF DONE)
     2. When creating a new customer loction, POST that to the CMMSSNS
         application.
         - if CMMSSNS creates a new branch, should it be sent down or
@@ -115,8 +145,10 @@ async def customer(
         branches (at least one).
     4. Add ancillary required records (i.e. customer payment tems, 
       logo file, special prepaid frieght terms, etc.)
-    5. If files are required (i.e. images), upload them.
+    5. If files are required (i.e. images), upload them. (DONE)
 """
+
+
 @customers.post(
         '',
         response_model=CustomerResponse,
@@ -128,8 +160,64 @@ async def new_customer(
         token: CustomersPerm,
         new_customer: NewCustomer
     ) -> CustomerResponse:
-    raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
+    """
+        Creates a new customer.
+        If the customer exists in the CMMSSNS database,
+            then use that id number.
+        If the customer does not exist, 
+            call out to the CMMSSNS service to create
+            a new customer entity and use the id returned.
+    """
+    customer_perm = token.permissions.get('customers')
+    authorized = customer_perm >= auth.CustomersPermPriority.sca_employee
+    if authorized:
+        ...
+    raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
+@customers.patch(
+        '/{customer_id}/customer-logo',
+        response_model=CustomerResponse,
+        tags=['file-upload']
+    )
+async def upload_customer_logo_file(
+        session: NewSession,
+        token: CustomersPerm,
+        customer_id: int,
+        file: UploadFile
+    ) -> CustomerResponse:
+    """
+        Special path with the same structure as a related object query
+        but it expects a file sent as form data in the body.
+
+        Returns the JSON:API Customer resource object, although the
+            path does not accept JSON:API spec query params nor a
+            resource object as part of the request.
+    """
+    customer_perm = token.permissions.get('customers')
+    authorized = customer_perm >= auth.CustomersPermPriority.sca_employee
+    if authorized:
+        new_logo = File(
+            file_content=await file.read(),
+            file_name=file.filename,
+            file_mime=file.content_type
+        )
+        file_dest = f'{API_TYPE}/{customer_id}/logo/{new_logo.file_name}'
+        await S3.upload_file(file=new_logo, destination=file_dest)
+        result = await mod_customer(
+            session=session,
+            token=token,
+            customer_id=customer_id,
+            mod_customer=ModCustomer(
+                data={
+                    'id': customer_id,
+                    'attributes': {
+                        'logo': file_dest
+                    }
+                }
+            )
+        )
+        return result
+    raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
 @customers.patch(
         '/{customer_id}',
@@ -143,7 +231,16 @@ async def mod_customer(
         customer_id: int,
         mod_customer: ModCustomer
     ) -> CustomerResponse:
-    raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
+    customer_perm = token.permissions.get('customers')
+    if customer_perm >= auth.CustomersPermPriority.sca_employee:
+        result = serializer.patch_resource(
+            session=session,
+            json_data=mod_customer.model_dump(),
+            api_type=API_TYPE,
+            obj_id=customer_id
+        )
+        return result.data
+    raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
 @customers.delete('/{customer_id}',tags=['jsonapi'])
 async def del_customer(
