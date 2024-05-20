e33eb7d1619e2307b96c7f136e19110baac66014
carboni-joseph <joe@carbonitech.com>
2024-05-20 13:57:56 -0400

    moved quotes route

--- app/adp/__init__.py
+++ app/adp/__init__.py
@@ -5,6 +5,6 @@ from app.adp.routes.adp_ah_programs import ah_progs
 from app.adp.routes.adp_program_parts import prog_parts
 from app.adp.routes.adp_program_ratings import prog_ratings
 from app.adp.routes.ratings_management import ratings_admin
-from app.adp.quotes.quotes import quotes as adp_quotes
+from app.adp.routes.quotes import quotes as adp_quotes
 from app.adp.quotes.job_quotes.relationships import quote_rel as adp_quote_rel
 import app.adp.main as api
--- app/adp/quotes/__init__.py
+++
@@ -1,2 +0,0 @@
-from app.adp.quotes.quotes import quotes
-from app.adp.quotes.job_quotes.relationships import quote_rel
