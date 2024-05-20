927accca5d9217ac50f5a0ae7db83e880b163304
carboni-joseph <joe@carbonitech.com>
2024-05-20 14:00:57 -0400

    mostly formatting but changed how helper functions treat new models. commiting to DB moved to secured route operations in `auth`

--- app/adp/extraction/models.py
+++ app/adp/extraction/models.py
@@ -8,37 +8,58 @@ from openpyxl.worksheet.worksheet import Worksheet
 from app.adp.adp_models import MODELS, S, Fields, ModelSeries
 from app.adp.utils.validator import Validator
 from app.db import ADP_DB, Stage, Session
-import warnings; warnings.simplefilter('ignore')
+import warnings
+
+warnings.simplefilter("ignore")
 
 # NOTE in `extract_models` replace with in-mem collection of files passed in from api
 TODAY = str(datetime.today().date())
+
 
 class ParsingModes(Enum):
     ATTRS_ONLY = auto()
     BASE_PRICE = auto()
     CUSTOMER_PRICING = auto()
 
+
 class InvalidParsingMode(Exception): ...
 
-def add_model_to_program(session: Session, adp_customer_id: int, model: str) -> int:
-    record_series = parse_model_string(session, adp_customer_id, model, ParsingModes.CUSTOMER_PRICING)
-    record_series['stage'] = Stage.PROPOSED.name
+
+def build_model_attributes(
+    session: Session, adp_customer_id: int, model: str
+) -> pd.Series:
+    record_series = parse_model_string(
+        session, adp_customer_id, model, ParsingModes.CUSTOMER_PRICING
+    )
+    record_series["stage"] = Stage.PROPOSED.name
+    record_series["effective_date"] = TODAY
+    record_series = record_series.dropna()
+    return record_series
     with session:
-        return separate_by_product_type_and_commit_to_db(session=session, data=record_series)
-    
-def parse_model_string(session: Session, adp_customer_id: int, model: str, mode: ParsingModes) -> pd.Series:
+        return separate_by_product_type_and_commit_to_db(
+            session=session, data=record_series
+        )
+
+
+def parse_model_string(
+    session: Session, adp_customer_id: int, model: str, mode: ParsingModes
+) -> pd.Series:
     model_obj: ModelSeries = None
     for m in MODELS:
         if matched_model := Validator(session, model, m).is_model():
             model_obj = matched_model
     if not model_obj:
-        raise Exception(f'No match found for model number {model}')
+        raise Exception(f"No match found for model number {model}")
     record = model_obj.record()
     record_series = pd.Series(record)
     match mode:
         case ParsingModes.CUSTOMER_PRICING:
-            record_series['customer_id'] = adp_customer_id
-            return price_models_by_customer_discounts(session=session, model=record_series, adp_customer_id=adp_customer_id)
+            record_series["customer_id"] = adp_customer_id
+            priced_model = price_models_by_customer_discounts(
+                session=session, model=record_series, adp_customer_id=adp_customer_id
+            )
+            priced_model.pop("customer_id")
+            return priced_model
         case ParsingModes.BASE_PRICE:
             return record_series
         case ParsingModes.ATTRS_ONLY:
@@ -50,14 +71,14 @@ def parse_model_string(session: Session, adp_customer_id: int, model: str, mode:
 
 def extract_models_from_sheet(session: Session, sheet: Worksheet) -> list[ModelSeries]:
     """iterates over cell contents
-        and uses Validator to see if the content appears to be
-        a valid ADP part number.
-        If content matches, add it to a list of model objects
+    and uses Validator to see if the content appears to be
+    a valid ADP part number.
+    If content matches, add it to a list of model objects
 
-        model_clean: ModelSeries
+    model_clean: ModelSeries
 
-        Returns: list[ModelSeries]
-        """
+    Returns: list[ModelSeries]
+    """
     models = list()
     for row in sheet:
         for col in range(sheet.max_column):
@@ -68,16 +89,17 @@ def extract_models_from_sheet(session: Session, sheet: Worksheet) -> list[ModelS
             for m in MODELS:
                 if model_clean := Validator(session, val, m).is_model():
                     if isinstance(model_clean, S):
-                        if model_clean.get('heat') == 'XX':
-                            for heat_option in ('05', '07', '10'):
+                        if model_clean.get("heat") == "XX":
+                            for heat_option in ("05", "07", "10"):
                                 model_variant = copy.deepcopy(model_clean)
-                                model_variant['heat'] = heat_option
+                                model_variant["heat"] = heat_option
                                 models.append(model_variant)
                         else:
-                            models.append(model_clean) 
+                            models.append(model_clean)
                     else:
                         models.append(model_clean)
     return models
+
 
 def extract_models_from_file(session: Session, file: str) -> set[ModelSeries]:
     wb = opxl.load_workbook(file)
@@ -86,52 +108,68 @@ def extract_models_from_file(session: Session, file: str) -> set[ModelSeries]:
         models.extend(extract_models_from_sheet(session=session, sheet=sheet))
     return set(models)
 
-def price_models_by_customer_discounts(session: Session, model: pd.Series, adp_customer_id: int) -> pd.Series:
-    mat_grp_discounts = ADP_DB.load_df(session=session, table_name='material_group_discounts', customer_id=adp_customer_id)
-    snps = ADP_DB.load_df(session=session, table_name='snps', customer_id=adp_customer_id).drop_duplicates()
 
-    no_disc_price = int(model['zero_discount_price'])
-    mat_group: str = model['mpg']
+def price_models_by_customer_discounts(
+    session: Session, model: pd.Series, adp_customer_id: int
+) -> pd.Series:
+    mat_grp_discounts = ADP_DB.load_df(
+        session=session,
+        table_name="material_group_discounts",
+        customer_id=adp_customer_id,
+    )
+    snps = ADP_DB.load_df(
+        session=session, table_name="snps", customer_id=adp_customer_id
+    ).drop_duplicates()
+
+    no_disc_price = int(model["zero_discount_price"])
+    mat_group: str = model["mpg"]
     mat_group_disc: pd.Series = mat_grp_discounts.loc[
         (mat_grp_discounts[Fields.CUSTOMER_ID.value] == adp_customer_id)
-        & (mat_grp_discounts['mat_grp'] == mat_group),
-        'discount'
+        & (mat_grp_discounts["mat_grp"] == mat_group),
+        "discount",
     ]
     mat_group_disc = mat_group_disc.item() if not mat_group_disc.empty else 0
-    if not isinstance(model[Fields.PRIVATE_LABEL.value],str):
+    if not isinstance(model[Fields.PRIVATE_LABEL.value], str):
         model_num: str = model[Fields.MODEL_NUMBER.value]
     else:
         model_num: str = model[Fields.PRIVATE_LABEL.value]
 
     snp: pd.Series = snps.loc[
         (snps[Fields.CUSTOMER_ID.value] == adp_customer_id)
-        & (snps['model'] == model_num)
-        & (snps['stage'].isin([Stage.ACTIVE.name, Stage.PROPOSED.name])),
-        'price'
+        & (snps["model"] == model_num)
+        & (snps["stage"].isin([Stage.ACTIVE.name, Stage.PROPOSED.name])),
+        "price",
     ]
     snp = snp.item() if not snp.empty else 0
-    snp_disc = f'{(1 - (snp / no_disc_price)) * 100:.1f}' if snp else 0
+    snp_disc = f"{(1 - (snp / no_disc_price)) * 100:.1f}" if snp else 0
     mat_group_price = no_disc_price * (1 - mat_group_disc / 100)
     mat_group_price = int(math.floor(mat_group_price + 0.5))
     mat_group_price = 0 if mat_group_price == no_disc_price else mat_group_price
     result = {
-        Fields.MATERIAL_GROUP_DISCOUNT.value: mat_group_disc if mat_group_disc else None,
-        Fields.MATERIAL_GROUP_NET_PRICE.value: mat_group_price if mat_group_price else None,
+        Fields.MATERIAL_GROUP_DISCOUNT.value: (
+            mat_group_disc if mat_group_disc else None
+        ),
+        Fields.MATERIAL_GROUP_NET_PRICE.value: (
+            mat_group_price if mat_group_price else None
+        ),
         Fields.SNP_DISCOUNT.value: snp_disc if snp_disc else None,
         Fields.SNP_PRICE.value: snp if snp else None,
-        Fields.NET_PRICE.value: min([price for price in (snp, mat_group_price, no_disc_price) if price])
+        Fields.NET_PRICE.value: min(
+            [price for price in (snp, mat_group_price, no_disc_price) if price]
+        ),
     }
     model.update(result)
     return model
 
+
 def separate_by_product_type_and_commit_to_db(session: Session, data: pd.Series) -> int:
-    if data['motor']:
-        table = 'ah_programs'
+    if data["motor"]:
+        table = "ah_programs"
     else:
-        table = 'coil_programs'
+        table = "coil_programs"
 
     model = data.dropna()
-    model['effective_date'] = TODAY
+    model["effective_date"] = TODAY
     cols = [f'"{col}"' for col in model.index.values.tolist()]
     vals = []
     for val in model.values.tolist():
@@ -143,16 +181,21 @@ def separate_by_product_type_and_commit_to_db(session: Session, data: pd.Series)
     sql = f"""INSERT INTO {table} ({','.join(cols)})
             VALUES ({','.join(vals)})
             RETURNING id;"""
-    new_id = ADP_DB.execute(session=session, sql=sql, params=dict(columns=cols, values=vals)).fetchone()[0]
+    new_id = ADP_DB.execute(
+        session=session, sql=sql, params=dict(columns=cols, values=vals)
+    ).fetchone()[0]
     session.commit()
     return new_id
 
+
 def reprice_programs(session: Session) -> None:
-    aliases = ADP_DB.load_df(session=session, table_name='customers')[['customer', 'adp_alias']]
+    aliases = ADP_DB.load_df(session=session, table_name="customers")[
+        ["customer", "adp_alias"]
+    ]
     for customer in aliases.itertuples():
         sca_customer_name = customer.customer
         adp_customer_name = customer.adp_alias
-        for table in ('coil_programs', 'ah_programs'):
+        for table in ("coil_programs", "ah_programs"):
             prog_data = ADP_DB.load_df(session=session, table_name=table)
         ...
 
@@ -181,7 +224,6 @@ def reprice_programs(session: Session) -> None:
     #     model_sales_by_year['total'] = model_sales_by_year.sum()
     #     model_sales_by_year = model_sales_by_year.apply(lambda val: f'{val:.2f}')
     #     return model_sales_by_year
-    
 
     # program_data[
     #     [Fields.SALES_2022.value,
