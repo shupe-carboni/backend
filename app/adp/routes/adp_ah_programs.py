a782b47cb09b4d53e8ca5b0aa73d84361094ca24
carboni-joseph <joe@carbonitech.com>
2024-05-21 11:15:20 -0400

    impl post and delete with auth secop class

--- app/adp/routes/adp_ah_programs.py
+++ app/adp/routes/adp_ah_programs.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import HTTPException, Depends, status
 from fastapi.routing import APIRouter
+from functools import partial
 
 from app import auth
 from app.db import Session, ADP_DB
@@ -10,16 +11,19 @@ from app.adp.models import (
     AHProgQueryJSONAPI,
     NewAHRObj,
     ModStageAHReq,
+    NewAHRObjFull,
 )
-from app.jsonapi.sqla_models import serializer, ADPAHProgram
+from app.adp.extraction.models import build_model_attributes
+from app.jsonapi.sqla_models import ADPAHProgram, ADPCustomer
 from app.jsonapi.core_models import convert_query
 
 ADP_AIR_HANDLERS_RESOURCE = ADPAHProgram.__jsonapi_type_override__
+ADP_CUSTOMERS_RESOURCE = ADPCustomer.__jsonapi_type_override__
 ah_progs = APIRouter(
     prefix=f"/{ADP_AIR_HANDLERS_RESOURCE}", tags=["air handlers", "programs"]
 )
 
-ADPPerm = Annotated[auth.VerifiedToken, Depends(auth.authenticate_auth0_token)]
+Token = Annotated[auth.VerifiedToken, Depends(auth.authenticate_auth0_token)]
 NewSession = Annotated[Session, Depends(ADP_DB.get_db)]
 converter = convert_query(AHProgQueryJSONAPI)
 
@@ -31,7 +35,7 @@ converter = convert_query(AHProgQueryJSONAPI)
     tags=["jsonapi"],
 )
 def all_ah_programs(
-    token: ADPPerm,
+    token: Token,
     session: NewSession,
     query: AirHandlerProgQuery = Depends(),
 ) -> AirHandlerProgResp:
@@ -39,14 +43,12 @@ def all_ah_programs(
     An SCA admin or employee will see all programs that exist.
     A customer will see only their own programs"""
     return (
-        auth.ADPOperations(token)
+        auth.ADPOperations(token, ADP_AIR_HANDLERS_RESOURCE)
         .allow_admin()
         .allow_sca()
         .allow_dev()
         .allow_customer("std")
-        .get(
-            session=session, resource=ADP_AIR_HANDLERS_RESOURCE, query=converter(query)
-        )
+        .get(session=session, query=converter(query))
     )
 
 
@@ -57,53 +59,68 @@ def all_ah_programs(
     tags=["jsonapi"],
 )
 def ah_program_product(
-    token: ADPPerm,
+    token: Token,
     session: NewSession,
     program_product_id: int,
     query: AirHandlerProgQuery = Depends(),
 ) -> AirHandlerProgResp:
     """get a specific product from the ah programs"""
     return (
-        auth.ADPOperations(token)
+        auth.ADPOperations(token, ADP_AIR_HANDLERS_RESOURCE)
         .allow_admin()
         .allow_sca()
         .allow_dev()
         .allow_customer("std")
         .get(
             session=session,
-            resource=ADP_AIR_HANDLERS_RESOURCE,
             query=converter(query),
             obj_id=program_product_id,
         )
     )
 
 
+def build_full_model_obj(session: Session, new_coil: NewAHRObj):
+    model_num = new_coil.attributes.model_number
+    adp_customer_id = new_coil.relationships.adp_customers.data.id
+    model_w_attrs = build_model_attributes(
+        session=session, model=model_num, adp_customer_id=adp_customer_id
+    )
+    json_api_data = dict(
+        data=NewAHRObjFull(
+            attributes=model_w_attrs.to_dict(),
+            relationships=new_coil.relationships,
+        ).model_dump(by_alias=True)
+    )
+    return json_api_data
+
+
 @ah_progs.post(
-    "/{adp_customer_id}",
+    "",
     response_model=AirHandlerProgResp,
     response_model_exclude_none=True,
     tags=["jsonapi"],
 )
 def add_to_ah_program(
-    token: ADPPerm,
+    token: Token,
     session: NewSession,
     adp_customer_id: int,
     new_ah: NewAHRObj,
 ) -> AirHandlerProgResp:
     """create a new product in an existing program"""
-    if token.permissions >= auth.Permissions.sca_employee:
-        model_num = new_ah.attributes.model_number
-        # new_id = add_model_to_program(session=session, model=model_num, adp_customer_id=adp_customer_id)
-        return serializer.get_resource(
+    json_api_data = partial(build_full_model_obj, session, new_ah)
+    adp_customer_id = new_ah.relationships.adp_customers.data.id
+    return (
+        auth.ADPOperations(token, ADP_AIR_HANDLERS_RESOURCE)
+        .allow_admin()
+        .allow_sca()
+        .allow_dev()
+        .allow_customer("std")
+        .post(
             session=session,
-            query=converter(AirHandlerProgQuery()),
-            api_type=ADP_AIR_HANDLERS_RESOURCE,
-            # obj_id=new_id,
-            obj_only=True,
-            permitted_ids=None,
+            data=json_api_data,
+            customer_id=adp_customer_id,
         )
-    else:
-        raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
+    )
 
 
 @ah_progs.patch(
@@ -113,21 +130,20 @@ def add_to_ah_program(
     tags=["jsonapi"],
 )
 def change_product_status(
-    token: ADPPerm,
+    token: Token,
     session: NewSession,
     program_product_id: int,
     new_stage: ModStageAHReq,
 ) -> AirHandlerProgResp:
     adp_customer_id = new_stage.data.relationships.adp_customers.data.id
     return (
-        auth.ADPOperations(token)
+        auth.ADPOperations(token, ADP_AIR_HANDLERS_RESOURCE)
         .allow_admin()
         .allow_sca()
         .allow_dev()
         .allow_customer("std")
         .patch(
             session=session,
-            resource=ADP_AIR_HANDLERS_RESOURCE,
             data=new_stage.model_dump(by_alias=True),
             customer_id=adp_customer_id,
             obj_id=program_product_id,
@@ -137,59 +153,54 @@ def change_product_status(
 
 @ah_progs.delete("/{program_product_id}", tags=["jsonapi"])
 def delete_ah_program_product(
-    token: ADPPerm,
-    session: NewSession,
-    program_product_id: int,
-):
-    if token.permissions >= auth.Permissions.sca_admin:
-        return serializer.delete_resource(
-            session, {}, ADP_AIR_HANDLERS_RESOURCE, program_product_id
-        )
-    else:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
-
-
-@ah_progs.get("/{program_product_id}/adp-customers")
-def get_related_customer(
-    token: ADPPerm,
+    token: Token,
     session: NewSession,
     program_product_id: int,
 ):
     return (
-        auth.ADPOperations(token)
+        auth.ADPOperations(token, ADP_AIR_HANDLERS_RESOURCE)
+        .allow_admin()
+        .delete(session=session, obj_id=program_product_id)
+    )
+
+
+@ah_progs.get("/{program_product_id}/" + ADP_CUSTOMERS_RESOURCE)
+def get_related_customer(
+    token: Token,
+    session: NewSession,
+    program_product_id: int,
+):
+    return (
+        auth.ADPOperations(token, ADP_AIR_HANDLERS_RESOURCE)
         .allow_admin()
         .allow_sca()
         .allow_dev()
         .allow_customer("std")
         .get(
             session=session,
-            resource=ADP_AIR_HANDLERS_RESOURCE,
-            query={},
             obj_id=program_product_id,
             relationship=False,
-            related_resource="adp-customers",
+            related_resource=ADP_CUSTOMERS_RESOURCE,
         )
     )
 
 
-@ah_progs.get("/{program_product_id}/relationships/adp-customers")
+@ah_progs.get("/{program_product_id}/relationships/" + ADP_CUSTOMERS_RESOURCE)
 def get_customer_relationship(
-    token: ADPPerm,
+    token: Token,
     session: NewSession,
     program_product_id: int,
 ):
     return (
-        auth.ADPOperations(token)
+        auth.ADPOperations(token, ADP_AIR_HANDLERS_RESOURCE)
         .allow_admin()
         .allow_sca()
         .allow_dev()
         .allow_customer("std")
         .get(
             session=session,
-            resource=ADP_AIR_HANDLERS_RESOURCE,
-            query={},
             obj_id=program_product_id,
             relationship=True,
-            related_resource="adp-customers",
+            related_resource=ADP_CUSTOMERS_RESOURCE,
         )
     )
