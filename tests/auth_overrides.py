676d7c1ce0796c6a346a9e09103497c7a52ad7b9
carboni-joseph <joe@carbonitech.com>
2024-05-23 07:15:43 -0400

    mock creation of new customer in CMMSSNS service

--- tests/auth_overrides.py
+++ tests/auth_overrides.py
@@ -1,8 +1,10 @@
 from app.auth import Permissions
 from dotenv import load_dotenv
+from app.customers.models import NewCMMSSNSCustomer, CMMSSNSCustomerResp
 
 load_dotenv()
 from os import getenv
+from random import randint
 
 
 class AdminToken:
@@ -37,3 +39,20 @@ class CustomerStandardToken:
     permissions = Permissions.customer_std
     email_verified = True
     email = getenv("TEST_USER_EMAIL")
+
+
+def mock_new_cmmssns_customer(new_customer: NewCMMSSNSCustomer) -> CMMSSNSCustomerResp:
+    return CMMSSNSCustomerResp(
+        jsonapi={"version": "123.456"},
+        meta={"client": "test"},
+        included=[],
+        data={
+            "id": randint(1000000, 9999999),
+            "type": "customers",
+            "attributes": {"name": new_customer.data.attributes.name},
+        },
+    )
+
+
+def mock_get_mock_new_cmmssns_customer():
+    return mock_new_cmmssns_customer
