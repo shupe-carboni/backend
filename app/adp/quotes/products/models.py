e07f1c66149128822d9b6f02027cd6c4ac576a60
carboni-joseph <joe@carbonitech.com>
2024-05-25 19:03:43 -0400

    black formatting plus defaul links field value set in RelatedProductResponse

--- app/adp/quotes/products/models.py
+++ app/adp/quotes/products/models.py
@@ -5,32 +5,38 @@ from app.jsonapi.core_models import (
     JSONAPIRelationshipsResponse,
     JSONAPIRelationships,
     JSONAPIResourceObject,
-    Pagination
+    Pagination,
 )
+
 
 class ProductResourceIdentifier(JSONAPIResourceIdentifier):
     type: str = "quote-products"
 
+
 class ProductRelationshipsResponse(JSONAPIRelationshipsResponse):
-    data: list[ProductResourceIdentifier]|ProductResourceIdentifier
+    data: list[ProductResourceIdentifier] | ProductResourceIdentifier
+
 
 ## Products & quantities associated with quotes
 # Schema
 class ProductAttributes(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
-    request_brand: Optional[str] = Field(alias='request-brand')
+    request_brand: Optional[str] = Field(alias="request-brand")
     product_tag_or_model: str = Field(alias="product-tag-or-model")
     product_model_quoted: Optional[str] = Field(alias="product-model-quoted")
     qty: int
     price: Optional[float] = None
 
+
 # Schema
 class ProductRelationships(BaseModel):
     adp_quote: JSONAPIRelationships
 
+
 class ProductResourceObject(ProductResourceIdentifier):
     attributes: ProductAttributes
     relationships: ProductRelationships
+
 
 class ProductResponse(BaseModel):
     meta: Optional[dict] = {}
@@ -38,17 +44,22 @@ class ProductResponse(BaseModel):
     included: Optional[list[JSONAPIResourceObject]]
     links: Optional[Pagination]
 
+
 class NewProductResourceObject(BaseModel):
     """add detail/products to an existing quote"""
+
     type: str
     attributes: ProductAttributes
     relationships: ProductRelationships
 
+
 class ExistingProduct(NewProductResourceObject):
-    id: str|int
+    id: str | int
+
 
 class RelatedProductResponse(ProductResponse):
     """When pulling as a related object, included is always empty
-        and links are not in the object"""
+    and links are not in the object"""
+
     included: dict = {}
-    links: dict = Field(..., exclude=True)
+    links: dict = Field(default=None, exclude=True)
