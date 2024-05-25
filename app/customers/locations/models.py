cced4747ee3fa7600454bc508a8c73d707ed6712
carboni-joseph <joe@carbonitech.com>
2024-05-25 19:04:31 -0400

    black formatting and data can be a single instance of a LocationResourceObject

--- app/customers/locations/models.py
+++ app/customers/locations/models.py
@@ -5,37 +5,46 @@ from app.jsonapi.core_models import (
     JSONAPIRelationshipsResponse,
     JSONAPIRelationships,
     JSONAPIResourceObject,
-    Pagination
+    Pagination,
 )
+
 
 class LocationResourceIdentifier(JSONAPIResourceIdentifier):
     type: str = "sca-customer-locations"
 
+
 class LocationRelationshipsResponse(JSONAPIRelationshipsResponse):
-    data: list[LocationResourceIdentifier]|LocationResourceIdentifier
+    data: list[LocationResourceIdentifier] | LocationResourceIdentifier
+
 
 ## Location
 # Schema
 class LocationAttrs(BaseModel):
     hq: bool
     dc: bool
+
+
 class LocationRelationships(BaseModel):
-    customers: JSONAPIRelationships = Field(alias='customers')
-    places: JSONAPIRelationships = Field(alias='places')
-    adp_quotes: JSONAPIRelationships = Field(alias='adp-quotes')
+    customers: JSONAPIRelationships = Field(alias="customers")
+    places: JSONAPIRelationships = Field(alias="places")
+    adp_quotes: JSONAPIRelationships = Field(alias="adp-quotes")
+
 
 class LocationResourceObject(LocationResourceIdentifier):
     attributes: LocationAttrs
     relationships: LocationRelationships
 
+
 class LocationResponse(BaseModel):
     meta: Optional[dict] = {}
-    data: Optional[list[LocationResourceObject]]
+    data: Optional[list[LocationResourceObject] | LocationResourceObject]
     included: Optional[list[JSONAPIResourceObject]]
     links: Optional[Pagination]
 
+
 class RelatedLocationResponse(LocationResponse):
     """When pulling as a related object, included is always empty
-        and links are not in the object"""
+    and links are not in the object"""
+
     included: dict = {}
     links: Optional[dict] = Field(default=None, exclude=True)
