fb8f49d555ff6b0184420c19b65b0efc303b9794
carboni-joseph <joe@carbonitech.com>
2024-06-05 11:54:28 -0400

    formatting

--- app/jsonapi/core_models.py
+++ app/jsonapi/core_models.py
@@ -8,14 +8,17 @@ from typing import Optional, Annotated, Any, Callable
 
 StringToNum = Annotated[int, BeforeValidator(lambda num: int(num))]
 
+
 class JSONAPIVersion(BaseModel):
     version: str
 
+
 class JSONAPIResourceObject(BaseModel):
-    id: str|int
+    id: str | int
     type: str
     attributes: Optional[dict] = {}
     relationships: Optional[dict] = {}
+
 
 class JSONAPIErrorObject(BaseModel):
     id: str
@@ -25,36 +28,45 @@ class JSONAPIErrorObject(BaseModel):
     title: str
     detail: str
 
+
 class JSONAPIErrorResponse(BaseModel):
     errors: list[JSONAPIErrorObject]
+
 
 class JSONAPIResponse(BaseModel):
     meta: Optional[dict] = {}
     data: Optional[JSONAPIResourceObject]
     included: Optional[list[JSONAPIResourceObject]] = []
 
+
 class JSONAPIResourceIdentifier(BaseModel):
-    id: int|str
+    id: int | str
     type: str
+
 
 class JSONAPIRelationshipsResponse(BaseModel):
     meta: Optional[dict] = {}
-    data: list[JSONAPIResourceIdentifier]|JSONAPIResourceIdentifier
+    data: list[JSONAPIResourceIdentifier] | JSONAPIResourceIdentifier
+
 
 class JSONAPIRelationshipLinks(BaseModel):
     self: str
     related: str
 
+
 class JSONAPIRelationships(BaseModel):
     links: Optional[JSONAPIRelationshipLinks] = None
-    data: Optional[JSONAPIResourceIdentifier|list[JSONAPIResourceIdentifier]] = None
+    data: Optional[JSONAPIResourceIdentifier | list[JSONAPIResourceIdentifier]] = None
+
 
 class JSONAPIRequestData(BaseModel):
     data: JSONAPIResourceObject
 
+
 class JSONAPIRequest(BaseModel):
     data: JSONAPIRequestData
     included: Optional[list[JSONAPIResourceObject]] = None
+
 
 class Pagination(BaseModel):
     # self: str
@@ -63,16 +75,23 @@ class Pagination(BaseModel):
     next: Optional[str] = None
     last: str
 
+
 class Query(BaseModel):
-    include: Optional[str|None] = None
+    include: Optional[str | None] = None
     sort: Optional[str] = None
     # fields: implemented at runtime
     # filter: implemented at runtime
     page_number: Optional[StringToNum] = None
     page_size: Optional[StringToNum] = None
 
+
 def convert_query(model_type: BaseModel) -> Callable[[BaseModel], dict[str, Any]]:
-    """Use the model_type as a converter to transform a query from it's pydantic type into a dict of JSONAPI arguments"""
+    """Use the model_type as a converter to transform a query from it's pydantic type
+    into a dict of JSONAPI arguments"""
+
     def inner(query_model: BaseModel) -> dict[str, Any]:
-        return model_type(**query_model.model_dump(exclude_none=True)).model_dump(by_alias=True, exclude_none=True)
+        return model_type(**query_model.model_dump(exclude_none=True)).model_dump(
+            by_alias=True, exclude_none=True
+        )
+
     return inner
