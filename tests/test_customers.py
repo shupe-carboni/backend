f5bd7dbf80700882d76b4490aca7b06162a8166f
carboni-joseph <joe@carbonitech.com>
2024-06-17 11:35:13 -0400

    refactor to use parameterization

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -1,21 +1,14 @@
-from fastapi.testclient import TestClient
+from pytest import mark
+from pathlib import Path
 from app.main import app
-from app.customers.models import CustomerResponse, NewCMMSSNSCustomer, ModCustomer
-from app.customers.locations.models import (
-    RelatedLocationResponse,
-    LocationRelationshipsResponse,
-)
-from app.adp.models import (
-    RelatedCustomerResponse,
-    CustomersRelResp,
-    RelatedADPCustomerTermsResp,
-    ADPCustomerTermsRelationshipsResp,
-)
+from fastapi.testclient import TestClient
+from app.customers.models import NewCMMSSNSCustomer, ModCustomer
 from app.auth import authenticate_auth0_token
 from app.customers.customers import get_new_cmmssns_customer_method
 from tests import auth_overrides
 from random import randint
 from pprint import pprint
+
 
 test_client = TestClient(app)
 CUSTOMER_ID = 999999  # NOTE associated with TEST CUSTOMER
@@ -24,247 +17,112 @@ app.dependency_overrides[get_new_cmmssns_customer_method] = (
     auth_overrides.mock_get_mock_new_cmmssns_customer
 )
 
+SCA_PERMS = (
+    auth_overrides.AdminToken,
+    auth_overrides.SCAEmployeeToken,
+)
+CUSTOMER_PERMS = (
+    auth_overrides.CustomerAdminToken,
+    auth_overrides.CustomerManagerToken,
+    auth_overrides.CustomerStandardToken,
+)
+DEV_PERM = auth_overrides.DeveloperToken
 
-def test_new_customer():
-    ROUTE = "/customers"
-    method = test_client.post
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
+ALL_ALLOWED = list(
+    zip(
+        [*SCA_PERMS, *CUSTOMER_PERMS, DEV_PERM],
+        [200] * (len(SCA_PERMS) + len(CUSTOMER_PERMS) + 1),
     )
-    response_codes = (200, 200, 401, 401, 401, 401)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        payload = NewCMMSSNSCustomer(
-            data={
-                "type": "customers",
-                "attributes": {"name": f"TEST {randint(1000, 9999)}"},
-            }
-        ).model_dump()
-        response = method(ROUTE, json=payload)
-        assert response.status_code == rc, pprint(response.json())
-        app.dependency_overrides[authenticate_auth0_token] = {}
+)
 
-
-def test_modify_customer():
-    ROUTE = f"/customers/{CUSTOMER_ID}"
-    method = test_client.patch
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
+SCA_ONLY_INCLUDING_DEV = list(
+    zip(
+        [*SCA_PERMS, *CUSTOMER_PERMS, DEV_PERM],
+        [200, 200, 401, 401, 401, 200],
     )
-    response_codes = (200, 200, 401, 401, 401, 401)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        payload = ModCustomer(
-            data={
-                "id": CUSTOMER_ID,
-                "type": "customers",
-                "attributes": {
-                    # "name": "TEST CUSTOMER",
-                    # "logo": "customers/999999/logo/py-color.jpg",
-                    "domains": ["somedomain.com", "someotherdomain.net"],
-                    "buying_group": "AD",
-                },
-            }
-        ).model_dump(exclude_none=True)
-        response = method(ROUTE, json=payload)
-        assert response.status_code == rc, f"{response.status_code}: {perm}"
-        if response.status_code == 200:
-            assert response.json()["data"]["attributes"].get("name") is not None
-        app.dependency_overrides[authenticate_auth0_token] = {}
+)
 
-
-def test_customer_collection():
-    ROUTE = "/customers"
-    method = test_client.get
-
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
+SCA_ONLY_EXCLUDING_DEV = list(
+    zip(
+        [*SCA_PERMS, *CUSTOMER_PERMS, DEV_PERM],
+        [200, 200, 401, 401, 401, 401],
     )
-    response_codes = (200, 200, 401, 401, 401, 401)
+)
 
-    data_lens = (None, None, 1, 1, 1, 1)
-    assert len(perms) == len(response_codes) == len(data_lens)
-    for perm, rc, dl in zip(perms, response_codes, data_lens):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        response = method(ROUTE)
-        assert response.status_code == rc
-        if rc == 200:
-            assert CustomerResponse(**response.json())
-            if dl:
-                assert len(response.json()["data"]) == dl
-            else:
-                assert len(response.json()["data"]) > 1
-        app.dependency_overrides[authenticate_auth0_token] = {}
-
-
-def test_customer_resource():
-    ROUTE = f"/customers/{CUSTOMER_ID}"
-    method = test_client.get
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
+SCA_ADMIN_ONLY = list(
+    zip(
+        [*SCA_PERMS, *CUSTOMER_PERMS, DEV_PERM],
+        [200, 401, 401, 401, 401, 401],
     )
-    response_codes = (200, 200, 401, 401, 401, 401)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        response = method(ROUTE)
-        assert response.status_code == rc
-        if rc == 200:
-            assert CustomerResponse(**response.json())
-        app.dependency_overrides[authenticate_auth0_token] = {}
+)
 
 
-def test_related_customer_locations():
-    ROUTE = f"/customers/{CUSTOMER_ID}/customer-locations"
-    method = test_client.get
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
+SCA_DEV_AND_CUSTOMER_ADMIN_ONLY = list(
+    zip(
+        [*SCA_PERMS, *CUSTOMER_PERMS, DEV_PERM],
+        [200, 200, 200, 401, 401, 200],
     )
-    response_codes = (200, 200, 401, 401, 401, 200)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        response = method(ROUTE)
-        assert response.status_code == rc
-        if rc == 200:
-            assert RelatedLocationResponse(**response.json())
-        app.dependency_overrides[authenticate_auth0_token] = {}
+)
+
+BASE_GETS = ["/customers", f"/customers/{CUSTOMER_ID}"]
+RELATED_GETS = [
+    BASE_GETS[-1] + f"/{related}"
+    for related in ["customer-locations", "adp-customers", "adp-customer-terms"]
+]
+RELATIONSHIP_GETS = [
+    BASE_GETS[-1] + f"/relationships/{related}"
+    for related in ["customer-locations", "adp-customers", "adp-customer-terms"]
+]
+GET_PATHS = [
+    (*perm_and_code, path)
+    for path in BASE_GETS + RELATED_GETS + RELATIONSHIP_GETS
+    for perm_and_code in SCA_DEV_AND_CUSTOMER_ADMIN_ONLY
+]
 
 
-def test_relationship_customer_locations():
-    ROUTE = f"/customers/{CUSTOMER_ID}/relationships/customer-locations"
-    method = test_client.get
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
-    )
-    response_codes = (200, 200, 401, 401, 401, 200)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        response = method(ROUTE)
-        assert response.status_code == rc
-        if rc == 200:
-            assert LocationRelationshipsResponse(**response.json())
-        app.dependency_overrides[authenticate_auth0_token] = {}
+@mark.parametrize("perm,response_code", SCA_ADMIN_ONLY)
+def test_new_customer_and_delete(perm, response_code):
+    ROUTE = Path("/customers")
+    app.dependency_overrides[authenticate_auth0_token] = perm
+    payload = NewCMMSSNSCustomer(
+        data={
+            "type": "customers",
+            "attributes": {"name": f"TEST {randint(1000, 9999)}"},
+        }
+    ).model_dump()
+    response = test_client.post(str(ROUTE), json=payload)
+    assert response.status_code == response_code, pprint(response.json())
+    if response_code != 200:
+        return
+    del_route = ROUTE / str(response.json()["data"]["id"])
+    response = test_client.delete(str(del_route))
+    assert response.status_code == 204, pprint(response.json())
 
 
-def test_related_adp_customers():
-    ROUTE = f"/customers/{CUSTOMER_ID}/adp-customers"
-    method = test_client.get
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
-    )
-    response_codes = (200, 200, 200, 401, 401, 200)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        response = method(ROUTE)
-        assert response.status_code == rc
-        if rc == 200:
-            assert RelatedCustomerResponse(**response.json())
-        app.dependency_overrides[authenticate_auth0_token] = {}
+@mark.parametrize("perm,response_code", SCA_ONLY_EXCLUDING_DEV)
+def test_modify_customer(perm, response_code):
+    ROUTE = Path("/customers") / str(CUSTOMER_ID)
+    app.dependency_overrides[authenticate_auth0_token] = perm
+    payload = ModCustomer(
+        data={
+            "id": CUSTOMER_ID,
+            "type": "customers",
+            "attributes": {
+                # "name": "TEST CUSTOMER",
+                # "logo": "customers/999999/logo/py-color.jpg",
+                "domains": ["somedomain.com", "someotherdomain.net"],
+                "buying-group": "AD",
+            },
+        }
+    ).model_dump(exclude_none=True, by_alias=True)
+    response = test_client.patch(str(ROUTE), json=payload)
+    assert response.status_code == response_code, pprint(response.json())
+    if response.status_code == 200:
+        assert response.json()["data"]["attributes"].get("name") is not None
 
 
-def test_relationship_adp_customers():
-    ROUTE = f"/customers/{CUSTOMER_ID}/relationships/adp-customers"
-    method = test_client.get
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
-    )
-    response_codes = (200, 200, 200, 401, 401, 200)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        response = method(ROUTE)
-        assert response.status_code == rc
-        if rc == 200:
-            assert CustomersRelResp(**response.json())
-        app.dependency_overrides[authenticate_auth0_token] = {}
-
-
-def test_related_adp_customer_terms():
-    ROUTE = f"/customers/{CUSTOMER_ID}/adp-customer-terms"
-    method = test_client.get
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
-    )
-    response_codes = (200, 200, 200, 401, 401, 200)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        response = method(ROUTE)
-        assert response.status_code == rc
-        if rc == 200:
-            assert RelatedADPCustomerTermsResp(**response.json()), pprint(
-                response.json()
-            )
-        app.dependency_overrides[authenticate_auth0_token] = {}
-
-
-def test_relationship_adp_customer_terms():
-    ROUTE = f"/customers/{CUSTOMER_ID}/relationships/adp-customer-terms"
-    method = test_client.get
-    perms = (
-        auth_overrides.AdminToken,
-        auth_overrides.SCAEmployeeToken,
-        auth_overrides.CustomerAdminToken,
-        auth_overrides.CustomerManagerToken,
-        auth_overrides.CustomerStandardToken,
-        auth_overrides.DeveloperToken,
-    )
-    response_codes = (200, 200, 200, 401, 401, 200)
-    assert len(perms) == len(response_codes)
-    for perm, rc in zip(perms, response_codes):
-        app.dependency_overrides[authenticate_auth0_token] = perm
-        response = method(ROUTE)
-        assert response.status_code == rc
-        if rc == 200:
-            assert ADPCustomerTermsRelationshipsResp(**response.json())
-        app.dependency_overrides[authenticate_auth0_token] = {}
+@mark.parametrize("perm,response_code,path", GET_PATHS)
+def test_customer_gets(perm, response_code, path):
+    app.dependency_overrides[authenticate_auth0_token] = perm
+    response = test_client.get(path)
+    assert response.status_code == response_code
