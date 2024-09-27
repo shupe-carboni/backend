6e8816f0c2e557718b0a6d249adbb209913dd6a4
carboni-joseph <joe@carbonitech.com>
2024-09-27 15:26:30 -0400

    unprotecting to confirm functionality. using auth in MS Power Automate isn't straightforward for client credentials

--- app/hardcast/routes.py
+++ app/hardcast/routes.py
@@ -1,7 +1,7 @@
 import logging
 import traceback
-from typing import Annotated
-from fastapi import HTTPException, status, Depends, UploadFile
+from typing import Annotated, Optional
+from fastapi import HTTPException, status, Depends, UploadFile, Header
 from fastapi.routing import APIRouter
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
@@ -18,12 +18,20 @@ NewSession = Annotated[Session, Depends(DB_V2.get_db)]
 @hardcast.post("/confirmations")
 async def new_confirmation(
     session: NewSession,
-    token: Token,
+    # token: Token,
     file: UploadFile,
+    authorization: Optional[str] = Header(None),
 ) -> JSONResponse:
-    if token.permissions != auth.Permissions.hardcast_confirmations:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
+    # if token.permissions != auth.Permissions.hardcast_confirmations:
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    if authorization:
+        warn_msg = (
+            "Endpoint unprotected but an auth header was sent, "
+            f"starting with {authorization[:30]}"
+        )
+    else:
+        warn_msg = "Endpoint unprotected and no auth header was sent."
+    logger.warning(warn_msg)
     new_conf = File(file.filename, file.content_type, await file.read())
     try:
         confirmation = confirmations.extract_from_file(new_conf.file_content)
