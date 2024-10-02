2ef0c8685fccba9b96da8bbe346989baed145105
carboni-joseph <joe@carbonitech.com>
2024-10-02 11:46:45 -0400

    test that endpoint is running through successfully

---
+++ tests/test_hardcast_confirmation_analysis.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from base64 import b64encode
+from app.main import app
+
+test_client = TestClient(app)
+PATH = "/hardcast/confirmations"
+test_file = "./tests/test_hardcast_conf.PDF"
+
+
+def test_confirmation_endpoint_works():
+    with open(test_file, "rb") as fh:
+        file_data: str = b64encode(fh.read()).decode()
+    pl = {"file": file_data, "content_type": "application/pdf"}
+    resp = test_client.post(PATH, json=pl)
+    assert resp.status_code == 200, resp.text
