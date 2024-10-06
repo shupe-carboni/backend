c98d06b8208c68986ac610fb0f6bd1edc4e780f5
carboni-joseph <joe@carbonitech.com>
2024-10-06 13:15:09 -0400

    test that the flow name is gaurding the ep and returning 401 when the flow name is missing or wrong

--- tests/test_hardcast_confirmation_analysis.py
+++ tests/test_hardcast_confirmation_analysis.py
@@ -1,17 +1,29 @@
+from os import getenv
 from fastapi.testclient import TestClient
 from base64 import b64encode
 from app.main import app
+from dotenv import load_dotenv
+
+load_dotenv()
 
 test_client = TestClient(app)
 PATH = "/hardcast/confirmations"
 test_file = "./tests/test_hardcast_conf.PDF"
+FLOW_NAME = getenv("FLOW_NAME")
 
 
 def test_confirmation_endpoint_works():
     with open(test_file, "rb") as fh:
         file_data: str = b64encode(fh.read()).decode()
+
     pl = {"file": file_data, "content_type": "application/pdf"}
-    resp = test_client.post(PATH, json=pl)
+    headers = {"x-ms-workflow-name": FLOW_NAME}
+    resp = test_client.post(PATH, json=pl, headers=headers)
     assert resp.status_code == 200, resp.text
     with open("email_test.html", "w") as email:
         email.write(resp.text)
+    resp = test_client.post(PATH, json=pl)
+    assert resp.status_code == 401, resp.text
+    headers = {"x-ms-workflow-name": "xyz123"}
+    resp = test_client.post(PATH, json=pl, headers=headers)
+    assert resp.status_code == 401, resp.text
