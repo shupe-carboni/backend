42dc345557e145e2c552f7598e28489c2651b4b1
carboni-joseph <joe@carbonitech.com>
2024-10-22 10:55:19 -0400

    added oem series

--- app/adp/utils/pricebook.py
+++ app/adp/utils/pricebook.py
@@ -622,7 +622,7 @@ class PriceBook:
         def set_series_name(oemseries: str) -> str:
             # TODO abstract this to either environment variables, db entries, or a file
             # extrnal to the code itself
-            if any([val in oemseries for val in ("4AC", "4HP")]):
+            if any([val in oemseries for val in ("4AC", "4HP", "7AC", "7HP")]):
                 start, end = (0, 5)
             elif (
                 oemseries.startswith("YH")
