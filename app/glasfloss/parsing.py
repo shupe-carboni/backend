8ef4e52f3bbe0bbbe35516729a608ec84fb373b5
carboni-joseph <joe@carbonitech.com>
2025-02-20 16:16:54 -0500

    implement made-to-order pricing and some refactoring

--- app/glasfloss/parsing.py
+++ app/glasfloss/parsing.py
@@ -93,23 +93,15 @@ class FilterBuilt(BaseModel):
     face_area: float = Field(alias="face-area")
     double_size: bool = Field(alias="double-size")
     qty_per_case: int = Field(alias="qty-per-case")
-    carton_weight: str = Field(alias="carton-weight")
+    carton_weight: Optional[str] = Field(default=None, alias="carton-weight")
     list_price: float = Field(alias="list-price")
-    multiplier: float = Field(alias="multiplier")
+    multiplier: float = Field(alias="effective-multiplier")
+    customer_multiplier: float = Field(alias="customer-multiplier")
     net_price: float = Field(alias="net-price")
-    net_price_broken_pallet: Optional[float] = Field(
-        default=None, alias="net-price-broken-pallet"
+    net_price_broken_case: Optional[float] = Field(
+        default=None, alias="net-price-broken-case"
     )
     effective_date: datetime = Field(alias="effective-date")
-
-
-@dataclass
-class Pricing:
-    list_price: int
-    multiplier: float
-    net_price: int
-    net_price_broken_pallet: int
-    effective_date: datetime
 
 
 FRACTION_CODES = {
@@ -176,6 +168,7 @@ class FilterModel:
         self.exact = filter_dims.exact
         self.exact_nomen = "E" if filter_dims.exact else ""
         self.multiplier = 1
+        self.customer_multiplier = 1
         self.calculated = False
 
         width_part = int(self.width)
@@ -222,7 +215,9 @@ class FilterModel:
                     self.double = True
                     self.multiplier = 1.5
                 else:
-                    self.double = False
+                    self.multiplier = 1.3
+                    self.double = True
+                self.broken_case_multiplier = 1.4
             case ModelType.ZLP:
                 invalid_depth = self.depth not in ALLOWED_DEPTHS[ModelType.ZLP]
                 below_min = self.width < 4 and self.height < 4
@@ -236,6 +231,7 @@ class FilterModel:
                     self.double = True
                 else:
                     self.double = False
+                self.broken_case_multiplier = 1.4
             case ModelType.HVP:
                 invalid_depth = self.depth not in ALLOWED_DEPTHS[ModelType.HVP]
                 below_min = self.width < 4 and self.height < 4
@@ -251,6 +247,7 @@ class FilterModel:
                     self.double = False
                 if depth_part == 6:
                     self.multiplier = 1.55
+                self.broken_case_multiplier = 1.4
             case ModelType.M11:
                 invalid_depth = self.depth not in ALLOWED_DEPTHS[ModelType.M11]
                 below_min = self.width < 4 and self.height < 4
@@ -266,6 +263,7 @@ class FilterModel:
                     self.double = False
                 if depth_part == 6:
                     self.multiplier = 1.55
+                self.broken_case_multiplier = 1.4
             case ModelType.M13:
                 invalid_depth = self.depth not in ALLOWED_DEPTHS[ModelType.M13]
                 below_min = self.width < 4 and self.height < 4
@@ -281,6 +279,7 @@ class FilterModel:
                     self.double = False
                 if depth_part == 6:
                     self.multiplier = 1.55
+                self.broken_case_multiplier = 1.4
             case _:
                 raise Exception("Invalid Model Type")
 
@@ -304,6 +303,88 @@ class FilterModel:
 
         If a customer_id is passed, also return the customer's multiplier and net price
         """
+
+        def set_multipliers(standard: bool) -> None:
+            customer_product_class_multiplier_sql = """
+                SELECT (1-discount) as multiplier, effective_date
+                FROM vendor_product_class_discounts a
+                WHERE EXISTS (
+                    SELECT 1
+                    FROM vendor_customers b
+                    WHERE a.vendor_customer_id = b.id
+                    AND b.vendor_id = 'glasfloss'
+                )
+                AND EXISTS (
+                    SELECT 1
+                    FROM vendor_product_classes c
+                    WHERE c.rank = 3
+                    AND c.name = :rank_3_name
+                    AND c.vendor_id = 'glasfloss'
+                    AND c.id = a.product_class_id
+                )
+                AND a.vendor_customer_id = :customer_id;
+            """
+            customer_product_multiplier_sql = """
+                SELECT (1-discount) as multiplier, effective_date
+                FROM vendor_product_discounts a
+                WHERE EXISTS (
+                    SELECT 1
+                    FROM vendor_customers b
+                    WHERE a.vendor_customer_id = b.id
+                    AND b.vendor_id = 'glasfloss'
+                )
+                AND EXISTS (
+                    SELECT 1
+                    FROM vendor_products c
+                    WHERE c.vendor_product_identifier = :model_number
+                    AND c.vendor_id = 'glasfloss'
+                    AND c.id = a.product_id
+                )
+                AND a.vendor_customer_id = :customer_id;
+            """
+            if not customer_id:
+                return
+            match self.model_type:
+                case ModelType.GDS if standard:
+                    if int(self.depth) in (1, 2):
+                        rank_3_name = f"{int(self.depth)} GDS DISPOSABLE"
+                case ModelType.GDS if not standard:
+                    rank_3_name = "GTA MADE-TO-ORDER DISPOSABLE"
+                case ModelType.HVP if standard:
+                    if int(self.depth) in (1, 2, 4):
+                        rank_3_name = f"HV {int(self.depth)} PLEATS"
+                case ModelType.HVP if not standard:
+                    rank_3_name = f"HV - MADE-TO-ORDER"
+                case ModelType.ZLP if standard:
+                    if int(self.depth) in (1, 2, 4):
+                        rank_3_name = f"ZL {int(self.depth)} PLEAT"
+                case ModelType.ZLP if not standard:
+                    if int(self.depth) in (1, 2, 4):
+                        rank_3_name = f"{int(self.depth)} ZL MADE-TO-ORDER"
+                    elif int(self.depth) == 6:
+                        rank_3_name = f"4 ZL MADE-TO-ORDER"
+                case ModelType.M11 if standard:
+                    if int(self.depth) in (1, 2, 4):
+                        rank_3_name = f"MR-11 STANDARD SIZE {int(self.depth)}"
+                case ModelType.M11 if not standard:
+                    rank_3_name = f"MR-11 PLEAT - MADE-TO-ORDER"
+                case ModelType.M13 if standard:
+                    if int(self.depth) in (1, 2, 4):
+                        rank_3_name = f"MR-13 STANDARD SIZE {int(self.depth)}"
+                case ModelType.M13 if not standard:
+                    rank_3_name = f"MR-13 PLEAT - MADE-TO-ORDER"
+
+            customer_multiplier = DB_V2.execute(
+                self.session,
+                sql=customer_product_class_multiplier_sql,
+                params=dict(rank_3_name=rank_3_name, customer_id=customer_id),
+            ).one_or_none()
+            if customer_multiplier:
+                multiplier, self.effective_date = customer_multiplier
+                self.customer_multiplier = multiplier
+                self.multiplier *= multiplier
+            return
+
         standard_filter_sql = """
             SELECT product_id, price, effective_date
             FROM vendor_pricing_by_class
@@ -317,43 +398,7 @@ class FilterModel:
                 AND a.id = vendor_pricing_by_class.product_id
                 AND a.vendor_product_identifier = :model_number);
         """
-        customer_product_class_multiplier_sql = """
-            SELECT (1-discount) as multiplier, effective_date
-            FROM vendor_product_class_discounts a
-            WHERE EXISTS (
-                SELECT 1
-                FROM vendor_customers b
-                WHERE a.vendor_customer_id = b.id
-                AND b.vendor_id = 'glasfloss'
-            )
-            AND EXISTS (
-                SELECT 1
-                FROM vendor_product_classes c
-                WHERE c.rank = 3
-                AND c.name = :rank_3_name
-                AND c.vendor_id = 'glasfloss'
-                AND c.id = a.product_class_id
-            )
-            AND a.vendor_customer_id = :customer_id;
-        """
-        customer_product_multiplier_sql = """
-            SELECT (1-discount) as multiplier, effective_date
-            FROM vendor_product_discounts a
-            WHERE EXISTS (
-                SELECT 1
-                FROM vendor_customers b
-                WHERE a.vendor_customer_id = b.id
-                AND b.vendor_id = 'glasfloss'
-            )
-            AND EXISTS (
-                SELECT 1
-                FROM vendor_products c
-                WHERE c.vendor_product_identifier = :model_number
-                AND c.vendor_id = 'glasfloss'
-                AND c.id = a.product_id
-            )
-            AND a.vendor_customer_id = :customer_id;
-        """
+
         standard_filter = DB_V2.execute(
             session=self.session,
             sql=standard_filter_sql,
@@ -382,44 +427,42 @@ class FilterModel:
             }
             self.qty_per_case = attrs.get("qty_per_case")
             self.carton_weight = attrs.get("carton_weight")
-            self.list_price = price / 100
+            self.list_price = price
             self.effective_date = effective_date
-
-            if customer_id:
-                match self.model_type:
-                    case ModelType.GDS:
-                        if int(self.depth) in (1, 2):
-                            rank_3_name = f"{int(self.depth)} GDS DISPOSABLE"
-                    case ModelType.HVP:
-                        if int(self.depth) in (1, 2, 4):
-                            rank_3_name = f"HV {int(self.depth)} PLEATS"
-                    case ModelType.ZLP:
-                        if int(self.depth) in (1, 2, 4):
-                            rank_3_name = f"ZL {int(self.depth)} PLEAT"
-                    case ModelType.M11:
-                        if int(self.depth) in (1, 2, 4):
-                            rank_3_name = f"MR-11 STANDARD SIZE {int(self.depth)}"
-                    case ModelType.M13:
-                        if int(self.depth) in (1, 2, 4):
-                            rank_3_name = f"MR-13 STANDARD SIZE {int(self.depth)}"
-
-                customer_multiplier = DB_V2.execute(
-                    self.session,
-                    sql=customer_product_class_multiplier_sql,
-                    params=dict(rank_3_name=rank_3_name, customer_id=customer_id),
-                ).one_or_none()
-                if customer_multiplier:
-                    multiplier, self.effective_date = customer_multiplier
-                    self.multiplier *= multiplier
-            self.net_price = self.list_price * self.multiplier
+            set_multipliers(standard=True)
         else:
-            self.prefix = self.model_series
-            self.qty_per_case = 0
-            self.carton_weight = ""
-            self.list_price = 0
+            depth_placeholder = self.depth if 4 >= self.depth else 4
+            mto_lookup_sql = """
+                SELECT price, part_number
+                FROM glasfloss_pricing_mto
+                WHERE series = :series
+                AND size_type = :size_type
+                AND depth = :depth
+                AND upper_bnd_face_area = (
+                    SELECT MIN(upper_bnd_face_area)
+                    FROM glasfloss_pricing_mto
+                    WHERE upper_bnd_face_area >= :face_area
+                );
+            """
+            result = DB_V2.execute(
+                self.session,
+                mto_lookup_sql,
+                params=dict(
+                    series=self.model_series,
+                    size_type="DOUBLE" if self.double else "SINGLE",
+                    depth=depth_placeholder,
+                    face_area=self.face_area,
+                ),
+            ).one()
+            (self.list_price, new_prefix) = result
+            self.prefix = new_prefix[:-2]
+            self.qty_per_case = 12
+            self.carton_weight = None
             self.effective_date = datetime.today()
-            self.net_price = 0
+            set_multipliers(standard=False)
 
+        self.net_price = self.list_price * self.multiplier
+        self.net_price_broken_case = self.net_price * self.broken_case_multiplier
         self.calculated = True
         return self
 
@@ -440,12 +483,17 @@ class FilterModel:
             double_size=self.double,
             qty_per_case=self.qty_per_case,
             carton_weight=self.carton_weight,
-            list_price=self.list_price,
-            multiplier=self.multiplier,
-            net_price=Decimal(self.net_price).quantize(
+            list_price=Decimal(self.list_price / 100).quantize(
                 ROUNDING_SIG, rounding=ROUND_HALF_UP
             ),
-            net_price_broken_pallet=None,
+            multiplier=self.multiplier,
+            customer_multiplier=self.customer_multiplier,
+            net_price=Decimal(self.net_price / 100).quantize(
+                ROUNDING_SIG, rounding=ROUND_HALF_UP
+            ),
+            net_price_broken_case=Decimal(self.net_price_broken_case / 100).quantize(
+                ROUNDING_SIG, rounding=ROUND_HALF_UP
+            ),
             effective_date=self.effective_date,
         )
         return FilterBuilt(**ret)
