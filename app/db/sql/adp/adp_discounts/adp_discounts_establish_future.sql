092bf81310c75adfbbda64f8d2f6d001a2178fd1
carboni-joseph <joe@carbonitech.com>
2025-05-02 09:40:14 -0400

    added a clarifying comment for myself

--- app/db/sql/adp/adp_discounts/adp_discounts_establish_future.sql
+++ app/db/sql/adp/adp_discounts/adp_discounts_establish_future.sql
@@ -7,7 +7,7 @@ SELECT vpc_discount.id, discount, :ed
 FROM adp_mgds
 JOIN vendor_product_classes vp_class
 	ON vp_class.name = adp_mgds.mg
-	AND vp_class.rank = 2
+	AND vp_class.rank = 2 -- STRATEGY_PRICING
 	AND vp_class.vendor_id = 'adp'
 JOIN vendor_customers vc
 	ON vc.name = adp_mgds.customer
