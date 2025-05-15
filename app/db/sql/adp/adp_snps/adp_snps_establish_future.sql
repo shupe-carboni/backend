edefba41530d0650c87461c4eda56ea1ed381770
carboni-joseph <joe@carbonitech.com>
2025-05-15 12:00:28 -0400

    pull correct id for the table with an additional join (probably a copy-paste error)

--- app/db/sql/adp/adp_snps/adp_snps_establish_future.sql
+++ app/db/sql/adp/adp_snps/adp_snps_establish_future.sql
@@ -3,10 +3,10 @@ INSERT INTO vendor_product_discounts_future (
 	discount,
 	effective_date
 )
-SELECT 
-	class_price.id,
-	(1-(new.price::float / class_price_future.price::float))*100, 
-	:ed
+SELECT DISTINCT
+	product_discounts.id discount_id,
+	(1-(new.price::float / class_price_future.price::float)) discount, 
+	:ed effective_date
 FROM adp_snps AS new
 JOIN vendor_customers vc
 	ON vc.name = new.customer
@@ -18,4 +18,8 @@ JOIN vendor_pricing_by_class AS class_price
 	ON class_price.product_id = vp.id
 JOIN vendor_pricing_by_class_future AS class_price_future
 	ON class_price_future.price_id = class_price.id
+JOIN vendor_product_discounts AS product_discounts
+	ON product_discounts.product_id = vp.id
+	AND product_discounts.product_id = class_price.product_id
+	AND product_discounts.vendor_customer_id = vc.id
 WHERE class_price_future.effective_date = :ed ;
